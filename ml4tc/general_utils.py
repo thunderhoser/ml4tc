314d557bea48e151f5cdab6659ba8ea7715a1584
thunderhoser <lagerqui@ualberta.ca>
2021-06-09 14:57:57 -0600

    FOO

--- ml4tc/general_utils.py
+++ ml4tc/general_utils.py
@@ -5,6 +5,7 @@ import sys
 import gzip
 import shutil
 import numpy
+from scipy.ndimage import distance_transform_edt
 
 THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
     os.path.join(os.getcwd(), os.path.expanduser(__file__))
@@ -71,3 +72,21 @@ def speed_and_heading_to_uv(storm_speeds_m_s01, storm_headings_deg):
     )
 
     return u_motions_m_s01, v_motions_m_s01
+
+
+def fill_nans(data_matrix):
+    """Fills NaN's with nearest neighbours.
+
+    This method is adapted from the method `fill`, which you can find here:
+    https://stackoverflow.com/posts/9262129/revisions
+
+    :param data_matrix: numpy array of real-valued data.
+    :return: data_matrix: Same but without NaN's.
+    """
+
+    error_checking.assert_is_real_numpy_array(data_matrix)
+
+    indices = distance_transform_edt(
+        numpy.isnan(data_matrix), return_distances=False, return_indices=True
+    )
+    return data_matrix[tuple(indices)]
