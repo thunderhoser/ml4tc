8701244ec5c5da1a8149a476ddcc363f50facf89
thunderhoser <lagerqui@ualberta.ca>
2021-06-16 11:52:29 -0600

    Almost ready to train models.

--- ml4tc/neural_net.py
+++ ml4tc/neural_net.py
@@ -5,14 +5,17 @@ import sys
 import copy
 import random
 import numpy
+import keras
 
 THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
     os.path.join(os.getcwd(), os.path.expanduser(__file__))
 ))
 sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
 
+import file_system_utils
 import error_checking
 import example_io
+import ships_io
 import example_utils
 import satellite_utils
 
@@ -24,6 +27,61 @@ DEFAULT_CLASS_CUTOFFS_KT = numpy.array(
     [-25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25], dtype=float
 )
 DEFAULT_CLASS_CUTOFFS_M_S01 = DEFAULT_CLASS_CUTOFFS_KT * KT_TO_METRES_PER_SECOND
+
+PLATEAU_PATIENCE_EPOCHS = 10
+DEFAULT_LEARNING_RATE_MULTIPLIER = 0.5
+PLATEAU_COOLDOWN_EPOCHS = 0
+EARLY_STOPPING_PATIENCE_EPOCHS = 30
+LOSS_PATIENCE = 0.
+
+EXAMPLE_DIRECTORY_KEY = 'example_dir_name'
+YEARS_KEY = 'years'
+LEAD_TIME_KEY = 'lead_time_hours'
+SATELLITE_LAG_TIMES_KEY = 'satellite_lag_times_minutes'
+SHIPS_LAG_TIMES_KEY = 'ships_lag_times_hours'
+SATELLITE_PREDICTORS_KEY = 'satellite_predictor_names'
+SHIPS_PREDICTORS_LAGGED_KEY = 'ships_predictor_names_lagged'
+SHIPS_PREDICTORS_FORECAST_KEY = 'ships_predictor_names_forecast'
+NUM_EXAMPLES_PER_BATCH_KEY = 'num_examples_per_batch'
+MAX_EXAMPLES_PER_CYCLONE_KEY = 'max_examples_per_cyclone_in_batch'
+CLASS_CUTOFFS_KEY = 'class_cutoffs_m_s01'
+
+KT_TO_METRES_PER_SECOND = 1.852 / 3.6
+
+DEFAULT_SATELLITE_PREDICTOR_NAMES = (
+    set(satellite_utils.FIELD_NAMES) -
+    set(example_utils.SATELLITE_METADATA_KEYS)
+)
+DEFAULT_SATELLITE_PREDICTOR_NAMES.remove(
+    satellite_utils.BRIGHTNESS_TEMPERATURE_KEY
+)
+DEFAULT_SATELLITE_PREDICTOR_NAMES = list(DEFAULT_SATELLITE_PREDICTOR_NAMES)
+
+DEFAULT_SHIPS_PREDICTOR_NAMES_LAGGED = (
+    set(ships_io.SATELLITE_FIELD_NAMES) -
+    set(example_utils.SHIPS_METADATA_KEYS)
+)
+DEFAULT_SHIPS_PREDICTOR_NAMES_LAGGED = list(
+    DEFAULT_SHIPS_PREDICTOR_NAMES_LAGGED
+)
+
+DEFAULT_SHIPS_PREDICTOR_NAMES_FORECAST = (
+    set(ships_io.FORECAST_FIELD_NAMES) -
+    set(example_utils.SHIPS_METADATA_KEYS)
+)
+DEFAULT_SHIPS_PREDICTOR_NAMES_FORECAST = list(
+    DEFAULT_SHIPS_PREDICTOR_NAMES_FORECAST
+)
+
+DEFAULT_GENERATOR_OPTION_DICT = {
+    LEAD_TIME_KEY: 24,
+    SATELLITE_PREDICTORS_KEY: DEFAULT_SATELLITE_PREDICTOR_NAMES,
+    SHIPS_PREDICTORS_LAGGED_KEY: DEFAULT_SHIPS_PREDICTOR_NAMES_LAGGED,
+    SHIPS_PREDICTORS_FORECAST_KEY: DEFAULT_SHIPS_PREDICTOR_NAMES_FORECAST,
+    NUM_EXAMPLES_PER_BATCH_KEY: 16,
+    MAX_EXAMPLES_PER_CYCLONE_KEY: 4,
+    CLASS_CUTOFFS_KEY: numpy.array([25 * KT_TO_METRES_PER_SECOND])
+}
 
 
 def _find_desired_times(
@@ -277,12 +335,76 @@ def _read_one_example_file(
     return predictor_matrices, target_array
 
 
-def input_generator(
-        example_dir_name, years, lead_time_hours, satellite_lag_times_minutes,
-        ships_lag_times_hours, satellite_predictor_names,
-        ships_predictor_names_lagged, ships_predictor_names_forecast,
-        num_examples_per_batch, max_examples_per_cyclone_in_batch,
-        class_cutoffs_m_s01):
+def _check_generator_args(option_dict):
+    """Error-checks input arguments for generator.
+
+    :param option_dict: See doc for `input_generator`.
+    :return: option_dict: Same as input, except defaults may have been added.
+    """
+
+    orig_option_dict = option_dict.copy()
+    option_dict = DEFAULT_GENERATOR_OPTION_DICT.copy()
+    option_dict.update(orig_option_dict)
+
+    error_checking.assert_is_integer_numpy_array(option_dict[YEARS_KEY])
+    error_checking.assert_is_numpy_array(
+        option_dict[YEARS_KEY], num_dimensions=1
+    )
+
+    error_checking.assert_is_integer(option_dict[LEAD_TIME_KEY])
+    assert numpy.mod(option_dict[LEAD_TIME_KEY], 6) == 0
+
+    error_checking.assert_is_integer_numpy_array(
+        option_dict[SATELLITE_LAG_TIMES_KEY]
+    )
+    error_checking.assert_is_geq_numpy_array(
+        option_dict[SATELLITE_LAG_TIMES_KEY], 0
+    )
+    error_checking.assert_is_numpy_array(
+        option_dict[SATELLITE_LAG_TIMES_KEY], num_dimensions=1
+    )
+
+    error_checking.assert_is_integer_numpy_array(
+        option_dict[SHIPS_LAG_TIMES_KEY]
+    )
+    error_checking.assert_is_geq_numpy_array(
+        option_dict[SHIPS_LAG_TIMES_KEY], 0
+    )
+    assert numpy.all(numpy.mod(option_dict[SHIPS_LAG_TIMES_KEY], 6) == 0)
+    error_checking.assert_is_numpy_array(
+        option_dict[SHIPS_LAG_TIMES_KEY], num_dimensions=1
+    )
+
+    # TODO(thunderhoser): Allow either list to be empty.
+    error_checking.assert_is_string_list(option_dict[SATELLITE_PREDICTORS_KEY])
+    error_checking.assert_is_string_list(
+        option_dict[SHIPS_PREDICTORS_LAGGED_KEY]
+    )
+    error_checking.assert_is_string_list(
+        option_dict[SHIPS_PREDICTORS_FORECAST_KEY]
+    )
+
+    error_checking.assert_is_integer(option_dict[NUM_EXAMPLES_PER_BATCH_KEY])
+    error_checking.assert_is_geq(option_dict[NUM_EXAMPLES_PER_BATCH_KEY], 10)
+    error_checking.assert_is_integer(option_dict[MAX_EXAMPLES_PER_CYCLONE_KEY])
+    error_checking.assert_is_geq(option_dict[MAX_EXAMPLES_PER_CYCLONE_KEY], 5)
+    error_checking.assert_is_greater(
+        option_dict[NUM_EXAMPLES_PER_BATCH_KEY],
+        option_dict[MAX_EXAMPLES_PER_CYCLONE_KEY]
+    )
+
+    error_checking.assert_is_numpy_array(
+        option_dict[CLASS_CUTOFFS_KEY], num_dimensions=1
+    )
+    error_checking.assert_is_greater_numpy_array(
+        numpy.diff(option_dict[CLASS_CUTOFFS_KEY]), 0.
+    )
+    assert numpy.all(numpy.isfinite(option_dict[CLASS_CUTOFFS_KEY]))
+
+    return option_dict
+
+
+def input_generator(option_dict):
     """Generates training data for neural net.
 
     E = number of examples per batch
@@ -294,25 +416,28 @@ def input_generator(
     C_ships = number of channels for SHIPS predictors
     K = number of classes
 
-    :param example_dir_name: Name of directory with example files.  Files
-        therein will be found by `example_io.find_file` and read by
+    :param option_dict: Dictionary with the following keys.
+    option_dict['example_dir_name']: Name of directory with example files.
+        Files therein will be found by `example_io.find_file` and read by
         `example_io.read_file`.
-    :param years: 1-D numpy array of training years.
-    :param lead_time_hours: Lead time for predicting storm intensity.
-    :param satellite_lag_times_minutes: 1-D numpy array of lag times for
+    option_dict['years']: 1-D numpy array of training years.
+    option_dict['lead_time_hours']: Lead time for predicting storm intensity.
+    option_dict['satellite_lag_times_minutes']: 1-D numpy array of lag times for
         satellite-based predictors.
-    :param ships_lag_times_hours: 1-D numpy array of lag times for SHIPS
+    option_dict['ships_lag_times_hours']: 1-D numpy array of lag times for SHIPS
         predictors.
-    :param satellite_predictor_names: 1-D list with names of satellite-based
-        predictors to use.
-    :param ships_predictor_names_lagged: 1-D list with names of lagged SHIPS
-        predictors to use.
-    :param ships_predictor_names_forecast: 1-D list with names of forecast SHIPS
-        predictors to use.
-    :param num_examples_per_batch: Number of examples per batch.
-    :param max_examples_per_cyclone_in_batch: Max number of examples (time
-        steps) from one cyclone in a batch.
-    :param class_cutoffs_m_s01: numpy array (length K - 1) of class cutoffs.
+    option_dict['satellite_predictor_names']: 1-D list with names of satellite-
+        based predictors to use.
+    option_dict['ships_predictor_names_lagged']: 1-D list with names of lagged
+        SHIPS predictors to use.
+    option_dict['ships_predictor_names_forecast']: 1-D list with names of
+        forecast SHIPS predictors to use.
+    option_dict['num_examples_per_batch']: Number of examples per batch.
+    option_dict['max_examples_per_cyclone_in_batch']: Max number of examples
+        (time steps) from one cyclone in a batch.
+    option_dict['class_cutoffs_m_s01']: numpy array (length K - 1) of class
+        cutoffs.
+
     :return: predictor_matrices: 1-D list with the following elements.
 
         brightness_temp_matrix: numpy array (E x M x N x T_sat x 1) of
@@ -328,6 +453,22 @@ def input_generator(
         (0 or 1), indicating true classes.  If K = 2, this is a length-E numpy
         array of integers (0 or 1).
     """
+
+    option_dict = _check_generator_args(option_dict)
+
+    example_dir_name = option_dict[EXAMPLE_DIRECTORY_KEY]
+    years = option_dict[YEARS_KEY]
+    lead_time_hours = option_dict[LEAD_TIME_KEY]
+    satellite_lag_times_minutes = option_dict[SATELLITE_LAG_TIMES_KEY]
+    ships_lag_times_hours = option_dict[SHIPS_LAG_TIMES_KEY]
+    satellite_predictor_names = option_dict[SATELLITE_PREDICTORS_KEY]
+    ships_predictor_names_lagged = option_dict[SHIPS_PREDICTORS_LAGGED_KEY]
+    ships_predictor_names_forecast = option_dict[SHIPS_PREDICTORS_FORECAST_KEY]
+    num_examples_per_batch = option_dict[NUM_EXAMPLES_PER_BATCH_KEY]
+    max_examples_per_cyclone_in_batch = (
+        option_dict[MAX_EXAMPLES_PER_CYCLONE_KEY]
+    )
+    class_cutoffs_m_s01 = option_dict[CLASS_CUTOFFS_KEY]
 
     # TODO(thunderhoser): For SHIPS predictors, all lag times and forecast times
     # are currently flattened along the channel axis.  I might change this in
@@ -444,3 +585,101 @@ def input_generator(
         target_array = target_array.astype('float16')
 
         yield predictor_matrices, target_array
+
+
+def train_model(
+        model_object, output_dir_name, num_epochs,
+        num_training_batches_per_epoch, training_option_dict,
+        num_validation_batches_per_epoch, validation_option_dict,
+        do_early_stopping=True,
+        plateau_lr_multiplier=DEFAULT_LEARNING_RATE_MULTIPLIER):
+    """Trains neural net.
+
+    :param model_object: Untrained neural net (instance of `keras.models.Model`
+        or `keras.models.Sequential`).
+    :param output_dir_name: Path to output directory (model and training history
+        will be saved here).
+    :param num_epochs: Number of training epochs.
+    :param num_training_batches_per_epoch: Number of training batches per epoch.
+    :param training_option_dict: See doc for `input_generator`.  This dictionary
+        will be used to generate training data.
+    :param num_validation_batches_per_epoch: Number of validation batches per
+        epoch.
+    :param validation_option_dict: See doc for `input_generator`.  For
+        validation only, the following values will replace corresponding values
+        in `training_option_dict`:
+
+    validation_option_dict['example_dir_name']
+    validation_option_dict['years']
+
+    :param do_early_stopping: Boolean flag.  If True, will stop training early
+        if validation loss has not improved over last several epochs (see
+        constants at top of file for what exactly this means).
+    :param plateau_lr_multiplier: Multiplier for learning rate.  Learning
+        rate will be multiplied by this factor upon plateau in validation
+        performance.
+    """
+
+    file_system_utils.mkdir_recursive_if_necessary(
+        directory_name=output_dir_name
+    )
+
+    error_checking.assert_is_integer(num_epochs)
+    error_checking.assert_is_geq(num_epochs, 2)
+    error_checking.assert_is_integer(num_training_batches_per_epoch)
+    error_checking.assert_is_geq(num_training_batches_per_epoch, 2)
+    error_checking.assert_is_integer(num_validation_batches_per_epoch)
+    error_checking.assert_is_geq(num_validation_batches_per_epoch, 2)
+    error_checking.assert_is_boolean(do_early_stopping)
+
+    if do_early_stopping:
+        error_checking.assert_is_greater(plateau_lr_multiplier, 0.)
+        error_checking.assert_is_less_than(plateau_lr_multiplier, 1.)
+
+    training_option_dict = _check_generator_args(training_option_dict)
+
+    validation_keys_to_keep = [EXAMPLE_DIRECTORY_KEY, YEARS_KEY]
+    for this_key in list(training_option_dict.keys()):
+        if this_key in validation_keys_to_keep:
+            continue
+
+        validation_option_dict[this_key] = training_option_dict[this_key]
+
+    validation_option_dict = _check_generator_args(validation_option_dict)
+
+    model_file_name = '{0:s}/model.h5'.format(output_dir_name)
+
+    history_object = keras.callbacks.CSVLogger(
+        filename='{0:s}/history.csv'.format(output_dir_name),
+        separator=',', append=False
+    )
+    checkpoint_object = keras.callbacks.ModelCheckpoint(
+        filepath=model_file_name, monitor='val_loss', verbose=1,
+        save_best_only=True, save_weights_only=False, mode='min', period=1
+    )
+    list_of_callback_objects = [history_object, checkpoint_object]
+
+    if do_early_stopping:
+        early_stopping_object = keras.callbacks.EarlyStopping(
+            monitor='val_loss', min_delta=LOSS_PATIENCE,
+            patience=EARLY_STOPPING_PATIENCE_EPOCHS, verbose=1, mode='min'
+        )
+        list_of_callback_objects.append(early_stopping_object)
+
+        plateau_object = keras.callbacks.ReduceLROnPlateau(
+            monitor='val_loss', factor=plateau_lr_multiplier,
+            patience=PLATEAU_PATIENCE_EPOCHS, verbose=1, mode='min',
+            min_delta=LOSS_PATIENCE, cooldown=PLATEAU_COOLDOWN_EPOCHS
+        )
+        list_of_callback_objects.append(plateau_object)
+
+    training_generator = input_generator(training_option_dict)
+    validation_generator = input_generator(validation_option_dict)
+
+    model_object.fit_generator(
+        generator=training_generator,
+        steps_per_epoch=num_training_batches_per_epoch,
+        epochs=num_epochs, verbose=1, callbacks=list_of_callback_objects,
+        validation_data=validation_generator,
+        validation_steps=num_validation_batches_per_epoch
+    )
---
+++ ml4tc/train_conv_lstm_test.py
@@ -0,0 +1,69 @@
+"""Trains test conv-LSTM model."""
+
+import os
+import sys
+import numpy
+import tensorflow.keras as tf_keras
+
+THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
+    os.path.join(os.getcwd(), os.path.expanduser(__file__))
+))
+sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
+
+import neural_net
+
+TEMPLATE_FILE_NAME = (
+    '/scratch1/RDARCH/rda-ghpcs/ml4tc_models/conv_lstm_test/'
+    'conv_lstm_test_template.h5'
+)
+OUTPUT_DIR_NAME = '/scratch1/RDARCH/rda-ghpcs/ml4tc_models/conv_lstm_test'
+EXAMPLE_DIR_NAME = (
+    '/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tc_project/'
+    'learning_examples/imputed/normalized'
+)
+
+KT_TO_METRES_PER_SECOND = 1.852 / 3.6
+TRAINING_YEARS = numpy.concatenate((
+    numpy.linspace(1993, 2004, num=12, dtype=int),
+    numpy.linspace(2015, 2019, num=5, dtype=int)
+))
+VALIDATION_YEARS = numpy.linspace(2005, 2009, num=5, dtype=int)
+
+TRAINING_OPTION_DICT = {
+    neural_net.EXAMPLE_DIRECTORY_KEY: EXAMPLE_DIR_NAME,
+    neural_net.YEARS_KEY: TRAINING_YEARS,
+    neural_net.LEAD_TIME_KEY: 24,
+    neural_net.SATELLITE_LAG_TIMES_KEY:
+        numpy.array([0, 60, 120, 180], dtype=int),
+    neural_net.SHIPS_LAG_TIMES_KEY: numpy.array([0, 6, 12, 18], dtype=int),
+    neural_net.NUM_EXAMPLES_PER_BATCH_KEY: 16,
+    neural_net.MAX_EXAMPLES_PER_CYCLONE_KEY: 4,
+    neural_net.CLASS_CUTOFFS_KEY: numpy.array([25 * KT_TO_METRES_PER_SECOND])
+}
+
+VALIDATION_OPTION_DICT = {
+    neural_net.EXAMPLE_DIRECTORY_KEY: EXAMPLE_DIR_NAME,
+    neural_net.YEARS_KEY: VALIDATION_YEARS
+}
+
+
+def _run():
+    """Trains test CNN.
+
+    This is effectively the main method.
+    """
+
+    model_object = tf_keras.models.load_model(TEMPLATE_FILE_NAME)
+
+    neural_net.train_model(
+        model_object=model_object, output_dir_name=OUTPUT_DIR_NAME,
+        num_epochs=100,
+        num_training_batches_per_epoch=32,
+        training_option_dict=TRAINING_OPTION_DICT,
+        num_validation_batches_per_epoch=16,
+        validation_option_dict=VALIDATION_OPTION_DICT,
+    )
+
+
+if __name__ == '__main__':
+    _run()
