511775aab1b90c1b0357c2b5410088c31be7f892
thunderhoser <lagerqui@ualberta.ca>
2021-07-21 14:31:52 -0600

    Screwing with saliency.

---
+++ ml4tc/machine_learning/saliency.py
@@ -0,0 +1,304 @@
+"""Methods for computing, reading, and writing saliency maps."""
+
+import numpy
+import netCDF4
+from gewittergefahr.gg_utils import file_system_utils
+from gewittergefahr.gg_utils import error_checking
+from gewittergefahr.deep_learning import saliency_maps as saliency_utils
+
+EXAMPLE_DIMENSION_KEY = 'example'
+CYCLONE_ID_CHAR_DIM_KEY = 'cyclone_id_char'
+GRID_ROW_DIMENSION_KEY = 'grid_row'
+GRID_COLUMN_DIMENSION_KEY = 'grid_column'
+SATELLITE_LAG_TIME_KEY = 'satellite_lag_time'
+GRIDDED_SATELLITE_CHANNEL_KEY = 'gridded_satellite_channel'
+UNGRIDDED_SATELLITE_CHANNEL_KEY = 'ungridded_satellite_channel'
+SHIPS_LAG_TIME_KEY = 'ships_lag_time'
+SHIPS_CHANNEL_KEY = 'ships_channel'
+
+CYCLONE_IDS_KEY = 'cyclone_id_strings'
+INIT_TIMES_KEY = 'init_times_unix_sec'
+GRIDDED_SATELLITE_SALIENCY_KEY = 'gridded_satellite_saliency_matrix'
+UNGRIDDED_SATELLITE_SALIENCY_KEY = 'ungridded_satellite_saliency_matrix'
+SHIPS_SALIENCY_KEY = 'ships_saliency_matrix'
+SALIENCY_KEY = 'saliency_matrices'
+
+MODEL_FILE_KEY = 'model_file_name'
+LAYER_NAME_KEY = 'layer_name'
+NEURON_INDICES_KEY = 'neuron_indices'
+IDEAL_ACTIVATION_KEY = 'ideal_activation'
+
+
+def check_metadata(layer_name, neuron_indices, ideal_activation):
+    """Checks metadata for errors.
+
+    :param layer_name: See doc for `get_saliency_one_neuron`.
+    :param neuron_indices: Same.
+    :param ideal_activation: Same.
+    """
+
+    error_checking.assert_is_string(layer_name)
+    error_checking.assert_is_integer_numpy_array(neuron_indices)
+    error_checking.assert_is_geq_numpy_array(neuron_indices, 0)
+    error_checking.assert_is_numpy_array(neuron_indices, num_dimensions=1)
+    error_checking.assert_is_not_nan(ideal_activation)
+
+
+def get_saliency_one_neuron(
+        model_object, predictor_matrices, layer_name, neuron_indices,
+        ideal_activation):
+    """Computes saliency maps with respect to activation of one neuron.
+
+    T = number of input tensors to model
+
+    The "relevant neuron" is that whose activation will be used in the numerator
+    of the saliency equation.  In other words, if the relevant neuron is n,
+    the saliency of each predictor x will be d(a_n) / dx, where a_n is the
+    activation of n.
+
+    :param model_object: Trained neural net (instance of `keras.models.Model` or
+        `keras.models.Sequential`).
+    :param predictor_matrices: length-T list, where each element is a numpy
+        array of predictors.  Predictors must be formatted in the same way as
+        for training.
+    :param layer_name: Name of layer with relevant neuron.
+    :param neuron_indices: 1-D numpy array with indices of relevant neuron.
+        Must have length D - 1, where D = number of dimensions in layer output.
+        The first dimension is the batch dimension, which always has length
+        `None` in Keras.
+    :param ideal_activation: Ideal neuron activation, used to define loss
+        function.  The loss function will be
+        (neuron_activation - ideal_activation)**2.
+    :return: saliency_matrices: length-T list, where each element is a numpy
+        array of saliency values.  saliency_matrices[i] will have the same shape
+        as predictor_matrices[i].
+    """
+
+    check_metadata(
+        layer_name=layer_name, neuron_indices=neuron_indices,
+        ideal_activation=ideal_activation
+    )
+
+    error_checking.assert_is_list(predictor_matrices)
+    for this_predictor_matrix in predictor_matrices:
+        error_checking.assert_is_numpy_array_without_nan(this_predictor_matrix)
+
+    activation_tensor = None
+
+    for k in neuron_indices[::-1]:
+        if activation_tensor is None:
+            activation_tensor = (
+                model_object.get_layer(name=layer_name).output[..., k]
+            )
+        else:
+            activation_tensor = activation_tensor[..., k]
+
+    loss_tensor = (activation_tensor - ideal_activation) ** 2
+
+    return saliency_utils.do_saliency_calculations(
+        model_object=model_object, loss_tensor=loss_tensor,
+        list_of_input_matrices=predictor_matrices
+    )[0]
+
+
+def write_file(
+        netcdf_file_name, saliency_matrices, cyclone_id_strings,
+        init_times_unix_sec, model_file_name, layer_name, neuron_indices,
+        ideal_activation):
+    """Writes saliency maps to NetCDF file.
+
+    E = number of examples
+    T = number of input tensors to model
+
+    :param netcdf_file_name: Path to output file.
+    :param saliency_matrices: length-T list, where each element is a numpy
+        array of saliency values.  saliency_matrices[i] should have the same
+        shape as the [i]th input tensor to the model.  Also, the first axis of
+        each numpy array must have length E.
+    :param cyclone_id_strings: length-E list of cyclone IDs.
+    :param init_times_unix_sec: length-E numpy array of forecast-init times.
+    :param model_file_name: Path to file with neural net used to create saliency
+        maps (readable by `neural_net.read_model`).
+    :param layer_name: See doc for `get_saliency_one_neuron`.
+    :param neuron_indices: Same.
+    :param ideal_activation: Same.
+    """
+
+    # Check input args.
+    check_metadata(
+        layer_name=layer_name, neuron_indices=neuron_indices,
+        ideal_activation=ideal_activation
+    )
+
+    error_checking.assert_is_list(saliency_matrices)
+    num_examples = -1
+
+    for i in range(len(saliency_matrices)):
+        error_checking.assert_is_numpy_array_without_nan(saliency_matrices[i])
+        if i == 0:
+            num_examples = saliency_matrices[i].shape[0]
+
+        expected_dim = numpy.array(
+            (num_examples,) + saliency_matrices[i].shape[1:], dtype=int
+        )
+        error_checking.assert_is_numpy_array(
+            saliency_matrices[i], exact_dimensions=expected_dim
+        )
+
+    expected_dim = numpy.array([num_examples], dtype=int)
+
+    error_checking.assert_is_string_list(cyclone_id_strings)
+    error_checking.assert_is_numpy_array(
+        numpy.array(cyclone_id_strings), exact_dimensions=expected_dim
+    )
+
+    error_checking.assert_is_integer_numpy_array(init_times_unix_sec)
+    error_checking.assert_is_numpy_array(
+        init_times_unix_sec, exact_dimensions=expected_dim
+    )
+
+    error_checking.assert_is_string(model_file_name)
+
+    # Write to NetCDF file.
+    file_system_utils.mkdir_recursive_if_necessary(file_name=netcdf_file_name)
+    dataset_object = netCDF4.Dataset(
+        netcdf_file_name, 'w', format='NETCDF3_64BIT_OFFSET'
+    )
+
+    dataset_object.setncattr(MODEL_FILE_KEY, model_file_name)
+    dataset_object.setncattr(LAYER_NAME_KEY, layer_name)
+    dataset_object.setncattr(NEURON_INDICES_KEY, neuron_indices)
+    dataset_object.setncattr(IDEAL_ACTIVATION_KEY, ideal_activation)
+
+    num_grid_rows = saliency_matrices[0].shape[1]
+    num_grid_columns = saliency_matrices[0].shape[2]
+    num_satellite_lag_times = saliency_matrices[0].shape[3]
+    num_gridded_satellite_channels = saliency_matrices[0].shape[4]
+    num_ungridded_satellite_channels = saliency_matrices[1].shape[2]
+    num_ships_lag_times = saliency_matrices[2].shape[1]
+    num_ships_channels = saliency_matrices[2].shape[2]
+
+    expected_dim = numpy.array([
+        num_examples, num_satellite_lag_times, num_ungridded_satellite_channels
+    ], dtype=int)
+    error_checking.assert_is_numpy_array(
+        saliency_matrices[1], exact_dimensions=expected_dim
+    )
+
+    dataset_object.createDimension(EXAMPLE_DIMENSION_KEY, num_examples)
+    dataset_object.createDimension(GRID_ROW_DIMENSION_KEY, num_grid_rows)
+    dataset_object.createDimension(GRID_COLUMN_DIMENSION_KEY, num_grid_columns)
+    dataset_object.createDimension(
+        SATELLITE_LAG_TIME_KEY, num_satellite_lag_times
+    )
+    dataset_object.createDimension(
+        GRIDDED_SATELLITE_CHANNEL_KEY, num_gridded_satellite_channels
+    )
+    dataset_object.createDimension(
+        UNGRIDDED_SATELLITE_CHANNEL_KEY, num_ungridded_satellite_channels
+    )
+    dataset_object.createDimension(SHIPS_LAG_TIME_KEY, num_ships_lag_times)
+    dataset_object.createDimension(SHIPS_CHANNEL_KEY, num_ships_channels)
+
+    if num_examples == 0:
+        num_id_characters = 1
+    else:
+        num_id_characters = numpy.max(numpy.array([
+            len(id) for id in cyclone_id_strings
+        ]))
+
+    dataset_object.createDimension(CYCLONE_ID_CHAR_DIM_KEY, num_id_characters)
+
+    this_string_format = 'S{0:d}'.format(num_id_characters)
+    cyclone_ids_char_array = netCDF4.stringtochar(numpy.array(
+        cyclone_id_strings, dtype=this_string_format
+    ))
+
+    dataset_object.createVariable(
+        CYCLONE_IDS_KEY, datatype='S1',
+        dimensions=(EXAMPLE_DIMENSION_KEY, CYCLONE_ID_CHAR_DIM_KEY)
+    )
+    dataset_object.variables[CYCLONE_IDS_KEY][:] = numpy.array(
+        cyclone_ids_char_array
+    )
+
+    dataset_object.createVariable(
+        INIT_TIMES_KEY, datatype=numpy.int32, dimensions=EXAMPLE_DIMENSION_KEY
+    )
+    dataset_object.variables[INIT_TIMES_KEY][:] = init_times_unix_sec
+
+    these_dim = (
+        EXAMPLE_DIMENSION_KEY, GRID_ROW_DIMENSION_KEY,
+        GRID_COLUMN_DIMENSION_KEY, SATELLITE_LAG_TIME_KEY,
+        GRIDDED_SATELLITE_CHANNEL_KEY
+    )
+    dataset_object.createVariable(
+        GRIDDED_SATELLITE_SALIENCY_KEY,
+        datatype=numpy.float32, dimensions=these_dim
+    )
+    dataset_object.variables[GRIDDED_SATELLITE_SALIENCY_KEY][:] = (
+        saliency_matrices[0]
+    )
+
+    these_dim = (
+        EXAMPLE_DIMENSION_KEY, SATELLITE_LAG_TIME_KEY,
+        UNGRIDDED_SATELLITE_CHANNEL_KEY
+    )
+    dataset_object.createVariable(
+        UNGRIDDED_SATELLITE_SALIENCY_KEY,
+        datatype=numpy.float32, dimensions=these_dim
+    )
+    dataset_object.variables[UNGRIDDED_SATELLITE_SALIENCY_KEY][:] = (
+        saliency_matrices[1]
+    )
+
+    these_dim = (
+        EXAMPLE_DIMENSION_KEY, SHIPS_LAG_TIME_KEY, SHIPS_CHANNEL_KEY
+    )
+    dataset_object.createVariable(
+        SHIPS_SALIENCY_KEY, datatype=numpy.float32, dimensions=these_dim
+    )
+    dataset_object.variables[SHIPS_SALIENCY_KEY][:] = saliency_matrices[2]
+
+    dataset_object.close()
+
+
+def read_file(netcdf_file_name):
+    """Reads saliency maps from NetCDF file.
+
+    :param netcdf_file_name: Path to input file.
+    :return: saliency_dict: Dictionary with the following keys.
+    saliency_dict['saliency_matrices']: See doc for `write_file`.
+    saliency_dict['cyclone_id_strings']: Same.
+    saliency_dict['init_times_unix_sec']: Same.
+    saliency_dict['model_file_name']: Same.
+    saliency_dict['layer_name']: Same.
+    saliency_dict['neuron_indices']: Same.
+    saliency_dict['ideal_activation']: Same.
+    """
+
+    dataset_object = netCDF4.Dataset(netcdf_file_name)
+
+    saliency_matrices = [
+        dataset_object.variables[GRIDDED_SATELLITE_SALIENCY_KEY][:],
+        dataset_object.variables[UNGRIDDED_SATELLITE_SALIENCY_KEY][:],
+        dataset_object.variables[SHIPS_SALIENCY_KEY][:]
+    ]
+
+    saliency_dict = {
+        SALIENCY_KEY: saliency_matrices,
+        CYCLONE_IDS_KEY: [
+            str(id) for id in
+            netCDF4.chartostring(dataset_object.variables[CYCLONE_IDS_KEY][:])
+        ],
+        INIT_TIMES_KEY: dataset_object.variables[INIT_TIMES_KEY][:],
+        MODEL_FILE_KEY: str(getattr(dataset_object, MODEL_FILE_KEY)),
+        LAYER_NAME_KEY: str(getattr(dataset_object, LAYER_NAME_KEY)),
+        NEURON_INDICES_KEY: numpy.array(
+            getattr(dataset_object, NEURON_INDICES_KEY), dtype=int
+        ),
+        IDEAL_ACTIVATION_KEY: getattr(dataset_object, IDEAL_ACTIVATION_KEY)
+    }
+
+    dataset_object.close()
+    return saliency_dict
---
+++ ml4tc/plotting/scalar_satellite_plotting.py
@@ -0,0 +1,277 @@
+"""Plots scalar (ungridded) satellite data."""
+
+import numpy
+import matplotlib
+matplotlib.use('agg')
+from matplotlib import pyplot
+from gewittergefahr.gg_utils import grids
+from gewittergefahr.gg_utils import time_conversion
+from gewittergefahr.gg_utils import error_checking
+from ml4tc.utils import example_utils
+from ml4tc.utils import satellite_utils
+
+TIME_FORMAT_SECONDS = '%Y-%m-%d-%H%M%S'
+TIME_FORMAT_MINUTES = '%Y-%m-%d-%H%M'
+
+MIN_NORMALIZED_VALUE = -3.
+MAX_NORMALIZED_VALUE = 3.
+COLOUR_MAP_OBJECT = pyplot.get_cmap('seismic')
+
+BAR_FACE_COLOUR = numpy.array([27, 158, 119], dtype=float) / 255
+BAR_EDGE_COLOUR = numpy.full(3, 0.)
+BAR_FONT_COLOUR = numpy.array([217, 95, 2], dtype=float) / 255
+BAR_EDGE_WIDTH = 2.
+BAR_FONT_SIZE = 20
+
+FIGURE_RESOLUTION_DPI = 300
+FIGURE_WIDTH_INCHES = 15
+FIGURE_HEIGHT_INCHES = 15
+
+DEFAULT_FONT_SIZE = 20
+VALID_TIME_FONT_SIZE = 12
+PREDICTOR_FONT_SIZE = 7
+
+pyplot.rc('font', size=DEFAULT_FONT_SIZE)
+pyplot.rc('axes', titlesize=DEFAULT_FONT_SIZE)
+pyplot.rc('axes', labelsize=DEFAULT_FONT_SIZE)
+pyplot.rc('xtick', labelsize=DEFAULT_FONT_SIZE)
+pyplot.rc('ytick', labelsize=DEFAULT_FONT_SIZE)
+pyplot.rc('legend', fontsize=DEFAULT_FONT_SIZE)
+pyplot.rc('figure', titlesize=DEFAULT_FONT_SIZE)
+
+
+def plot_bar_graph_one_time(
+        example_table_xarray, time_index, predictor_indices, info_string=None,
+        figure_object=None, axes_object=None):
+    """Plots predictors at one time as bar graph.
+
+    :param example_table_xarray: xarray table in format returned by
+        `example_io.read_file`.
+    :param time_index: Index of valid time to plot.
+    :param predictor_indices: 1-D numpy array with indices of predictors to
+        plot.
+    :param info_string: Info string (to be appended to title).
+    :param figure_object: Will plot on this figure (instance of
+        `matplotlib.figure.Figure`).  If None, will create new figure.
+    :param axes_object: Will plot on these axes (instance of
+        `matplotlib.axes._subplots.AxesSubplot`).  If None, will create new
+        axes.
+    :return: figure_object: See input doc.
+    :return: axes_object: See input doc.
+    """
+
+    error_checking.assert_is_integer(time_index)
+    error_checking.assert_is_geq(time_index, 0)
+    error_checking.assert_is_integer_numpy_array(predictor_indices)
+    error_checking.assert_is_geq_numpy_array(predictor_indices, 0)
+    if info_string is not None:
+        error_checking.assert_is_string(info_string)
+
+    xt = example_table_xarray
+    predictor_values = (
+        xt[example_utils.SATELLITE_PREDICTORS_UNGRIDDED_KEY].values[
+            time_index, predictor_indices
+        ]
+    )
+
+    num_predictors = len(predictor_values)
+    y_coords = numpy.linspace(
+        0, num_predictors - 1, num=num_predictors, dtype=float
+    )
+
+    if figure_object is None or axes_object is None:
+        figure_object, axes_object = pyplot.subplots(
+            1, 1, figsize=(FIGURE_WIDTH_INCHES, FIGURE_HEIGHT_INCHES)
+        )
+
+    axes_object.barh(
+        y_coords, predictor_values, color=BAR_FACE_COLOUR,
+        edgecolor=BAR_EDGE_COLOUR, linewidth=BAR_EDGE_WIDTH
+    )
+
+    pyplot.yticks([], [])
+    axes_object.set_xlim(MIN_NORMALIZED_VALUE, MAX_NORMALIZED_VALUE)
+
+    predictor_names = xt.coords[
+        example_utils.SATELLITE_PREDICTOR_UNGRIDDED_DIM
+    ].values[predictor_indices].tolist()
+
+    for j in range(num_predictors):
+        axes_object.text(
+            0, y_coords[j], predictor_names[j], color=BAR_FONT_COLOUR,
+            horizontalalignment='center', verticalalignment='center',
+            fontsize=BAR_FONT_SIZE, fontweight='bold'
+        )
+
+    valid_time_unix_sec = (
+        xt.coords[example_utils.SATELLITE_TIME_DIM].values[time_index]
+    )
+    valid_time_string = time_conversion.unix_sec_to_string(
+        valid_time_unix_sec, TIME_FORMAT_SECONDS
+    )
+    cyclone_id_string = xt[satellite_utils.CYCLONE_ID_KEY].values[time_index]
+    if not isinstance(cyclone_id_string, str):
+        cyclone_id_string = cyclone_id_string.decode('utf-8')
+
+    title_string = 'Satellite for {0:s} at {1:s}'.format(
+        cyclone_id_string, valid_time_string
+    )
+    if info_string is not None:
+        title_string += '; {0:s}'.format(info_string)
+
+    axes_object.set_title(title_string)
+
+    return figure_object, axes_object
+
+
+def plot_colour_map_multi_times(
+        example_table_xarray, time_indices, predictor_indices, info_string=None,
+        figure_object=None, axes_object=None):
+    """Plots predictors at many times as colour map.
+
+    :param example_table_xarray: See doc for `plot_bar_graph_one_time`.
+    :param time_indices: 1-D numpy array with indices of valid times to plot.
+    :param predictor_indices: See doc for `plot_bar_graph_one_time`.
+    :param info_string: Same.
+    :param figure_object: Same.
+    :param axes_object: Same.
+    :return: figure_object: Same.
+    :return: axes_object: Same.
+    """
+
+    error_checking.assert_is_integer_numpy_array(time_indices)
+    error_checking.assert_is_geq_numpy_array(time_indices, 0)
+    error_checking.assert_is_integer_numpy_array(predictor_indices)
+    error_checking.assert_is_geq_numpy_array(predictor_indices, 0)
+    if info_string is not None:
+        error_checking.assert_is_string(info_string)
+
+    # TODO(thunderhoser): Should I really be transposing?
+    xt = example_table_xarray
+    predictor_matrix = numpy.transpose(
+        xt[example_utils.SATELLITE_PREDICTORS_UNGRIDDED_KEY].values[
+            time_indices, :
+        ][:, predictor_indices]
+    )
+    predictor_matrix = predictor_matrix.astype(float)
+
+    valid_times_unix_sec = (
+        xt.coords[example_utils.SATELLITE_TIME_DIM].values[time_indices]
+    )
+    valid_time_strings = [
+        time_conversion.unix_sec_to_string(t, TIME_FORMAT_MINUTES)
+        for t in valid_times_unix_sec
+    ]
+    cyclone_id_strings = xt[satellite_utils.CYCLONE_ID_KEY].values[time_indices]
+    if not isinstance(cyclone_id_strings[0], str):
+        cyclone_id_strings = [s.decode('utf-8') for s in cyclone_id_strings]
+
+    cyclone_id_strings = numpy.unique(numpy.array(cyclone_id_strings))
+    assert len(cyclone_id_strings) == 1
+    cyclone_id_string = cyclone_id_strings[0]
+
+    if figure_object is None or axes_object is None:
+        figure_object, axes_object = pyplot.subplots(
+            1, 1, figsize=(FIGURE_WIDTH_INCHES, FIGURE_HEIGHT_INCHES)
+        )
+
+    colour_norm_object = pyplot.Normalize(
+        vmin=MIN_NORMALIZED_VALUE, vmax=MAX_NORMALIZED_VALUE
+    )
+    axes_object.imshow(
+        predictor_matrix, cmap=COLOUR_MAP_OBJECT, origin='lower',
+        norm=colour_norm_object
+    )
+
+    y_tick_values = numpy.linspace(
+        0, predictor_matrix.shape[0] - 1, num=predictor_matrix.shape[0],
+        dtype=float
+    )
+    y_tick_labels = ['{0:s}'.format(t) for t in valid_time_strings]
+    pyplot.yticks(
+        y_tick_values, y_tick_labels, fontsize=VALID_TIME_FONT_SIZE
+    )
+    axes_object.set_ylabel('Valid time')
+
+    x_tick_values = numpy.linspace(
+        0, predictor_matrix.shape[1] - 1, num=predictor_matrix.shape[1],
+        dtype=float
+    )
+    x_tick_labels = xt.coords[
+        example_utils.SATELLITE_PREDICTOR_UNGRIDDED_DIM
+    ].values[predictor_indices].tolist()
+
+    pyplot.xticks(
+        x_tick_values, x_tick_labels, rotation=90., fontsize=PREDICTOR_FONT_SIZE
+    )
+
+    title_string = 'Satellite for {0:s}'.format(cyclone_id_string)
+    if info_string is not None:
+        title_string += '; {0:s}'.format(info_string)
+
+    axes_object.set_title(title_string)
+
+    return figure_object, axes_object
+
+
+def plot_pm_signs_multi_times(
+        data_matrix, axes_object, font_size, colour_map_object,
+        max_absolute_colour_value):
+    """Plots data at many times with plus-minus signs.
+
+    Positive values will be plotted as plus signs; negative values will be
+    plotted as minus signs; and the colour map will be applied to absolute
+    values only.
+
+    This method is good for plotting saliency, input * gradient, or any other
+    explainable-ML result that can be either positive or negative.
+
+    V = number of satellite variables
+    T = number of times
+
+    :param data_matrix: V-by-T numpy array of data values to plot.
+    :param axes_object: Will plot on these axes (instance of
+        `matplotlib.axes._subplots.AxesSubplot`).
+    :param font_size: Font size for plus and minus signs.
+    :param colour_map_object: Colour scheme (instance of
+        `matplotlib.pyplot.cm`).
+    :param max_absolute_colour_value: Max absolute value in colour scheme.
+    """
+
+    error_checking.assert_is_numpy_array_without_nan(data_matrix)
+    error_checking.assert_is_numpy_array(data_matrix, num_dimensions=2)
+    error_checking.assert_is_geq(max_absolute_colour_value, 0.)
+    max_absolute_colour_value = max([max_absolute_colour_value, 0.001])
+
+    num_grid_rows = data_matrix.shape[0]
+    num_grid_columns = data_matrix.shape[1]
+    x_coord_spacing = num_grid_columns ** -1
+    y_coord_spacing = num_grid_rows ** -1
+
+    x_coords, y_coords = grids.get_xy_grid_points(
+        x_min_metres=x_coord_spacing / 2, y_min_metres=y_coord_spacing / 2,
+        x_spacing_metres=x_coord_spacing, y_spacing_metres=y_coord_spacing,
+        num_rows=num_grid_rows, num_columns=num_grid_columns
+    )
+
+    colour_norm_object = pyplot.Normalize(
+        vmin=0., vmax=max_absolute_colour_value
+    )
+    rgb_matrix = colour_map_object(colour_norm_object(
+        numpy.absolute(data_matrix)
+    ))[..., :-1]
+
+    for i in range(num_grid_rows):
+        for j in range(num_grid_columns):
+            if data_matrix[i, j] >= 0:
+                axes_object.text(
+                    x_coords[i], y_coords[j], '+', fontsize=font_size,
+                    color=rgb_matrix[i, j, ...], horizontalalignment='center',
+                    verticalalignment='center', transform=axes_object.transAxes
+                )
+            else:
+                axes_object.text(
+                    x_coords[i], y_coords[j], '_', fontsize=font_size,
+                    color=rgb_matrix[i, j, ...], horizontalalignment='center',
+                    verticalalignment='bottom', transform=axes_object.transAxes
+                )
