2ad08ab1eee9080309caa9c9a26ec02d01b8ccce
thunderhoser <lagerqui@ualberta.ca>
2021-07-22 16:55:47 -0600

    FOO

--- ml4tc/make_saliency_maps.py
+++ ml4tc/make_saliency_maps.py
@@ -5,6 +5,7 @@ import sys
 import copy
 import argparse
 import numpy
+import tensorflow
 
 THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
     os.path.join(os.getcwd(), os.path.expanduser(__file__))
