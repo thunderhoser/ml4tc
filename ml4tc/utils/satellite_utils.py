e9f8faa51e5bfb86b2a78648d7b1d0e61d09b6c0
thunderhoser <lagerqui@ualberta.ca>
2021-09-24 15:13:15 -0600

    Added code to properly center the goddamn brightness-temperature images at the storm center.

--- ml4tc/utils/satellite_utils.py
+++ ml4tc/utils/satellite_utils.py
@@ -3,10 +3,13 @@
 import warnings
 import numpy
 import xarray
+from scipy.interpolate import interp1d
+from gewittergefahr.gg_utils import number_rounding
+from gewittergefahr.gg_utils import longitude_conversion as lng_conversion
 from gewittergefahr.gg_utils import error_checking
 
-NUM_GRID_ROWS = 480
-NUM_GRID_COLUMNS = 640
+DEFAULT_NUM_GRID_ROWS = 480
+DEFAULT_NUM_GRID_COLUMNS = 640
 
 GRID_ROW_DIM = 'satellite_grid_row'
 GRID_COLUMN_DIM = 'satellite_grid_column'
@@ -71,6 +74,213 @@ VALID_BASIN_ID_STRINGS = [
     NORTHWEST_PACIFIC_ID_STRING, NORTH_INDIAN_ID_STRING,
     SOUTHERN_HEMISPHERE_ID_STRING
 ]
+
+
+def _find_storm_center_px_space(
+        storm_latitude_deg_n, storm_longitude_deg_e, grid_latitudes_deg_n,
+        grid_longitudes_deg_e):
+    """Finds storm center in pixel space.
+
+    M = number of rows in grid
+    N = number of columns in grid
+
+    :param storm_latitude_deg_n: Latitude (deg north) of storm center.
+    :param storm_longitude_deg_e: Longitude (deg east) of storm center.
+    :param grid_latitudes_deg_n: length-M numpy array of grid-point latitudes
+        (deg north).
+    :param grid_longitudes_deg_e: length-N numpy array of grid-point longitudes
+        (deg east).
+    :return: storm_row: Row index (half-integer) of storm center.
+    :return: storm_column: Column index (half-integer) of storm center.
+    """
+
+    num_rows = len(grid_latitudes_deg_n)
+    row_indices = numpy.linspace(0, num_rows - 1, num=num_rows, dtype=float)
+
+    interp_object = interp1d(
+        x=grid_latitudes_deg_n, y=row_indices, kind='linear',
+        bounds_error=True, assume_sorted=True
+    )
+    storm_row = number_rounding.round_to_half_integer(
+        interp_object(storm_latitude_deg_n)
+    )
+
+    num_columns = len(grid_longitudes_deg_e)
+    column_indices = numpy.linspace(
+        0, num_columns - 1, num=num_columns, dtype=float
+    )
+
+    grid_longitudes_deg_e = lng_conversion.convert_lng_positive_in_west(
+        grid_longitudes_deg_e, allow_nan=False
+    )
+    storm_longitude_deg_e = lng_conversion.convert_lng_positive_in_west(
+        storm_longitude_deg_e, allow_nan=False
+    )
+
+    if not numpy.all(numpy.diff(grid_longitudes_deg_e) > 0):
+        grid_longitudes_deg_e = lng_conversion.convert_lng_negative_in_west(
+            grid_longitudes_deg_e, allow_nan=False
+        )
+        storm_longitude_deg_e = lng_conversion.convert_lng_negative_in_west(
+            storm_longitude_deg_e, allow_nan=False
+        )
+
+    interp_object = interp1d(
+        x=grid_longitudes_deg_e, y=column_indices, kind='linear',
+        bounds_error=True, assume_sorted=True
+    )
+    storm_column = number_rounding.round_to_half_integer(
+        interp_object(storm_longitude_deg_e)
+    )
+
+    return storm_row, storm_column
+
+
+def _crop_image_around_storm_center(
+        data_matrix, grid_latitudes_deg_n, grid_longitudes_deg_e,
+        storm_row, storm_column, num_cropped_rows, num_cropped_columns):
+    """Crops satellite image around storm center.
+
+    M = number of grid rows before cropping
+    N = number of grid columns before cropping
+    m = number of grid rows after cropping
+    n = number of grid columns after cropping
+
+    :param data_matrix: 2-D numpy array of spatial data.
+    :param grid_latitudes_deg_n: length-M numpy array of grid-point latitudes
+        (deg north).
+    :param grid_longitudes_deg_e: length-N numpy array of grid-point longitudes
+        (deg east).
+    :param storm_row: Row index (half-integer) at storm center.
+    :param storm_column: Column index (half-integer) at storm center.
+    :param num_cropped_rows: m in the above discussion.  Must be even integer.
+    :param num_cropped_columns: n in the above discussion.  Must be even
+        integer.
+    :return: data_matrix: m-by-n numpy array of spatial data, centered at storm
+        center.
+    :return: grid_latitudes_deg_n: length-m numpy array of grid-point latitudes
+        (deg north).
+    :return: grid_longitudes_deg_e: length-n numpy array of grid-point longitudes
+        (deg east).
+    """
+
+    grid_longitudes_deg_e = lng_conversion.convert_lng_positive_in_west(
+        grid_longitudes_deg_e
+    )
+
+    if not numpy.all(numpy.diff(grid_longitudes_deg_e) > 0):
+        grid_longitudes_deg_e = lng_conversion.convert_lng_negative_in_west(
+            grid_longitudes_deg_e
+        )
+
+    num_total_rows = data_matrix.shape[0]
+    num_total_columns = data_matrix.shape[1]
+
+    error_checking.assert_is_integer(num_cropped_rows)
+    error_checking.assert_is_greater(num_cropped_rows, 0)
+    assert numpy.mod(num_cropped_rows, 2) == 0
+
+    error_checking.assert_is_integer(num_cropped_columns)
+    error_checking.assert_is_greater(num_cropped_columns, 0)
+    assert numpy.mod(num_cropped_columns, 2) == 0
+
+    half_num_cropped_rows = num_cropped_rows / 2 - 0.5
+    first_row = int(numpy.round(
+        storm_row - half_num_cropped_rows
+    ))
+    last_row = 1 + int(numpy.round(
+        storm_row + half_num_cropped_rows
+    ))
+
+    half_num_cropped_columns = num_cropped_columns / 2 - 0.5
+    first_column = int(numpy.round(
+        storm_column - half_num_cropped_columns
+    ))
+    last_column = 1 + int(numpy.round(
+        storm_column + half_num_cropped_columns
+    ))
+
+    if first_row < 0:
+        num_padding_rows = -first_row
+        padding_arg = ((num_padding_rows, 0), (0, 0))
+        data_matrix = numpy.pad(data_matrix, pad_width=padding_arg, mode='edge')
+
+        latitude_spacing_deg = numpy.diff(grid_latitudes_deg_n[:2])[0]
+        end_value_arg = (
+            grid_latitudes_deg_n[0] - num_padding_rows * latitude_spacing_deg,
+            0
+        )
+        grid_latitudes_deg_n = numpy.pad(
+            grid_latitudes_deg_n, pad_width=(num_padding_rows, 0),
+            mode='linear_ramp', end_values=end_value_arg
+        )
+
+        first_row += num_padding_rows
+        last_row += num_padding_rows
+        num_total_rows += num_padding_rows
+
+    if last_row > num_total_rows:
+        num_padding_rows = last_row - num_total_rows
+        padding_arg = ((0, num_padding_rows), (0, 0))
+        data_matrix = numpy.pad(data_matrix, pad_width=padding_arg, mode='edge')
+
+        latitude_spacing_deg = numpy.diff(grid_latitudes_deg_n[-2:])[0]
+        end_value_arg = (
+            0,
+            grid_latitudes_deg_n[-1] + num_padding_rows * latitude_spacing_deg
+        )
+        grid_latitudes_deg_n = numpy.pad(
+            grid_latitudes_deg_n, pad_width=(0, num_padding_rows),
+            mode='linear_ramp', end_values=end_value_arg
+        )
+
+        del num_total_rows
+
+    if first_column < 0:
+        num_padding_columns = -first_column
+        padding_arg = ((0, 0), (num_padding_columns, 0))
+        data_matrix = numpy.pad(data_matrix, pad_width=padding_arg, mode='edge')
+
+        longitude_spacing_deg = numpy.diff(grid_longitudes_deg_e[:2])[0]
+        end_value_arg = (
+            grid_longitudes_deg_e[0]
+            - num_padding_columns * longitude_spacing_deg,
+            0
+        )
+        grid_longitudes_deg_e = numpy.pad(
+            grid_longitudes_deg_e, pad_width=(num_padding_columns, 0),
+            mode='linear_ramp', end_values=end_value_arg
+        )
+
+        first_column += num_padding_columns
+        last_column += num_padding_columns
+        num_total_columns += num_padding_columns
+
+    if last_column > num_total_columns:
+        num_padding_columns = last_column - num_total_columns
+        padding_arg = ((0, 0), (0, num_padding_columns))
+        data_matrix = numpy.pad(data_matrix, pad_width=padding_arg, mode='edge')
+
+        longitude_spacing_deg = numpy.diff(grid_longitudes_deg_e[-2:])[0]
+        end_value_arg = (
+            grid_longitudes_deg_e[-1]
+            + num_padding_columns * longitude_spacing_deg,
+            0
+        )
+        grid_longitudes_deg_e = numpy.pad(
+            grid_longitudes_deg_e, pad_width=(0, num_padding_columns),
+            mode='linear_ramp', end_values=end_value_arg
+        )
+
+        del num_total_columns
+
+    return (
+        data_matrix[first_row:last_row, first_column:last_column],
+        grid_latitudes_deg_n[first_row:last_row],
+        lng_conversion.convert_lng_positive_in_west(
+            grid_longitudes_deg_e[first_column:last_column]
+        )
+    )
 
 
 def check_basin_id(basin_id_string):
@@ -152,9 +362,15 @@ def concat_tables_over_time(satellite_tables_xarray):
         len(t.coords[GRID_COLUMN_DIM].values) for t in satellite_tables_xarray
     ], dtype=int)
 
+    if (
+            len(numpy.unique(num_rows_by_table)) == 1 and
+            len(numpy.unique(num_columns_by_table)) == 1
+    ):
+        return xarray.concat(objs=satellite_tables_xarray, dim=TIME_DIM)
+
     good_flags = numpy.logical_and(
-        num_rows_by_table == NUM_GRID_ROWS,
-        num_columns_by_table == NUM_GRID_COLUMNS
+        num_rows_by_table == DEFAULT_NUM_GRID_ROWS,
+        num_columns_by_table == DEFAULT_NUM_GRID_COLUMNS
     )
     bad_indices = numpy.where(numpy.invert(good_flags))[0]
 
@@ -166,7 +382,7 @@ def concat_tables_over_time(satellite_tables_xarray):
         ).format(
             i + 1, len(satellite_tables_xarray),
             num_rows_by_table[i], num_columns_by_table[i],
-            NUM_GRID_ROWS, NUM_GRID_COLUMNS
+            DEFAULT_NUM_GRID_ROWS, DEFAULT_NUM_GRID_COLUMNS
         )
 
         warnings.warn(warning_string)
@@ -185,3 +401,100 @@ def concat_tables_over_time(satellite_tables_xarray):
         )
 
     return xarray.concat(objs=tables_to_concat, dim=TIME_DIM)
+
+
+def crop_images_around_storm_centers(
+        satellite_table_xarray, num_cropped_rows, num_cropped_columns):
+    """Crops satellite image around storm center.
+
+    M = number of grid rows after cropping
+    N = number of grid columns after cropping
+
+    :param satellite_table_xarray: xarray table in format returned
+        by `satellite_io.read_file`.
+    :param num_cropped_rows: M in the above discussion.  Must be even integer.
+    :param num_cropped_columns: N in the above discussion.  Must be even
+        integer.
+    :return: satellite_table_xarray: Same as input but with only M-by-N images.
+    """
+
+    error_checking.assert_is_integer(num_cropped_rows)
+    error_checking.assert_is_greater(num_cropped_rows, 0)
+    error_checking.assert_is_integer(num_cropped_columns)
+    error_checking.assert_is_greater(num_cropped_columns, 0)
+
+    bad_object_flags = numpy.logical_or(
+        numpy.isnan(satellite_table_xarray[STORM_LATITUDE_KEY].values),
+        numpy.isnan(satellite_table_xarray[STORM_LONGITUDE_KEY].values)
+    )
+    good_object_indices = numpy.where(
+        numpy.invert(bad_object_flags)
+    )[0]
+    satellite_table_xarray = satellite_table_xarray.isel(
+        indexers={TIME_DIM: good_object_indices}
+    )
+
+    num_storm_objects = len(satellite_table_xarray.coords[TIME_DIM].values)
+    brightness_temp_matrix_kelvins = numpy.full(
+        (num_storm_objects, num_cropped_rows, num_cropped_columns), numpy.nan
+    )
+    grid_latitude_matrix_deg_n = numpy.full(
+        (num_storm_objects, num_cropped_rows), numpy.nan
+    )
+    grid_longitude_matrix_deg_e = numpy.full(
+        (num_storm_objects, num_cropped_columns), numpy.nan
+    )
+
+    t = satellite_table_xarray
+
+    for i in range(num_storm_objects):
+        storm_row, storm_column = _find_storm_center_px_space(
+            storm_latitude_deg_n=t[STORM_LATITUDE_KEY].values[i],
+            storm_longitude_deg_e=t[STORM_LONGITUDE_KEY].values[i],
+            grid_latitudes_deg_n=t[GRID_LATITUDE_KEY].values[i, :],
+            grid_longitudes_deg_e=t[GRID_LONGITUDE_KEY].values[i, :]
+        )
+
+        (
+            brightness_temp_matrix_kelvins[i, ...],
+            grid_latitude_matrix_deg_n[i, :],
+            grid_longitude_matrix_deg_e[i, :]
+        ) = (
+            _crop_image_around_storm_center(
+                data_matrix=t[BRIGHTNESS_TEMPERATURE_KEY].values[i, ...],
+                grid_latitudes_deg_n=t[GRID_LATITUDE_KEY].values[i, :],
+                grid_longitudes_deg_e=t[GRID_LONGITUDE_KEY].values[i, :],
+                storm_row=storm_row, storm_column=storm_column,
+                num_cropped_rows=num_cropped_rows,
+                num_cropped_columns=num_cropped_columns
+            )
+        )
+
+    satellite_table_xarray = satellite_table_xarray.drop(
+        [BRIGHTNESS_TEMPERATURE_KEY, GRID_LATITUDE_KEY, GRID_LONGITUDE_KEY]
+    )
+
+    this_dict = {
+        GRID_ROW_DIM: numpy.linspace(
+            0, num_cropped_rows - 1, num=num_cropped_rows, dtype=int
+        ),
+        GRID_COLUMN_DIM: numpy.linspace(
+            0, num_cropped_columns - 1, num=num_cropped_columns, dtype=int
+        )
+    }
+    satellite_table_xarray = satellite_table_xarray.assign_coords(this_dict)
+
+    satellite_table_xarray[BRIGHTNESS_TEMPERATURE_KEY] = (
+        (TIME_DIM, GRID_ROW_DIM, GRID_COLUMN_DIM),
+        brightness_temp_matrix_kelvins
+    )
+    satellite_table_xarray[GRID_LATITUDE_KEY] = (
+        (TIME_DIM, GRID_ROW_DIM),
+        grid_latitude_matrix_deg_n
+    )
+    satellite_table_xarray[GRID_LONGITUDE_KEY] = (
+        (TIME_DIM, GRID_COLUMN_DIM),
+        grid_longitude_matrix_deg_e
+    )
+
+    return satellite_table_xarray
--- ml4tc/utils/satellite_utils_test.py
+++ ml4tc/utils/satellite_utils_test.py
@@ -1,8 +1,123 @@
 """Unit tests for satellite_utils.py."""
 
 import unittest
+import numpy
 from ml4tc.utils import satellite_utils
 
+TOLERANCE = 1e-6
+
+# The following constants are used to test _find_storm_center_px_space.
+GRID_LATITUDES_DEG_N = numpy.array(
+    [-10, -8, -6, -4, -2, 0, 3, 6, 9, 12, 20], dtype=float
+)
+GRID_LONGITUDES_DEG_E = numpy.array(
+    [350, 355, 0, 5, 10, 15, 25, 35, 45, 55, 65, 75], dtype=float
+)
+
+FIRST_STORM_LATITUDE_DEG_N = 6.9
+FIRST_STORM_LONGITUDE_DEG_E = 354.3
+FIRST_STORM_ROW = 7.5
+FIRST_STORM_COLUMN = 0.5
+
+SECOND_STORM_LATITUDE_DEG_N = 16.7
+SECOND_STORM_LONGITUDE_DEG_E = 26.3
+SECOND_STORM_ROW = 9.5
+SECOND_STORM_COLUMN = 6.5
+
+THIRD_STORM_LATITUDE_DEG_N = 11.9
+THIRD_STORM_LONGITUDE_DEG_E = 35.5
+THIRD_STORM_ROW = 8.5
+THIRD_STORM_COLUMN = 7.5
+
+FOURTH_STORM_LATITUDE_DEG_N = -1.5
+FOURTH_STORM_LONGITUDE_DEG_E = 51.4
+FOURTH_STORM_ROW = 4.5
+FOURTH_STORM_COLUMN = 8.5
+
+FIFTH_STORM_LATITUDE_DEG_N = 15.1
+FIFTH_STORM_LONGITUDE_DEG_E = 7.6
+FIFTH_STORM_ROW = 9.5
+FIFTH_STORM_COLUMN = 3.5
+
+# The following constants are used to test _crop_image_around_storm_center.
+UNCROPPED_DATA_MATRIX = numpy.array([
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+    [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
+    [37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48],
+    [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+    [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72],
+    [73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84],
+    [85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96],
+    [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108],
+    [109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120],
+    [121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132]
+], dtype=float)
+
+NUM_CROPPED_ROWS = 4
+NUM_CROPPED_COLUMNS = 6
+
+FIRST_CROPPED_DATA_MATRIX = numpy.array([
+    [73, 73, 73, 74, 75, 76],
+    [85, 85, 85, 86, 87, 88],
+    [97, 97, 97, 98, 99, 100],
+    [109, 109, 109, 110, 111, 112]
+], dtype=float)
+
+FIRST_CROPPED_LATITUDES_DEG_N = numpy.array([3, 6, 9, 12], dtype=float)
+FIRST_CROPPED_LONGITUDES_DEG_E = numpy.array(
+    [340, 345, 350, 355, 0, 5], dtype=float
+)
+
+SECOND_CROPPED_DATA_MATRIX = numpy.array([
+    [101, 102, 103, 104, 105, 106],
+    [113, 114, 115, 116, 117, 118],
+    [125, 126, 127, 128, 129, 130],
+    [125, 126, 127, 128, 129, 130]
+], dtype=float)
+
+SECOND_CROPPED_LATITUDES_DEG_N = numpy.array([9, 12, 20, 28], dtype=float)
+SECOND_CROPPED_LONGITUDES_DEG_E = numpy.array(
+    [10, 15, 25, 35, 45, 55], dtype=float
+)
+
+THIRD_CROPPED_DATA_MATRIX = numpy.array([
+    [90, 91, 92, 93, 94, 95],
+    [102, 103, 104, 105, 106, 107],
+    [114, 115, 116, 117, 118, 119],
+    [126, 127, 128, 129, 130, 131]
+], dtype=float)
+
+THIRD_CROPPED_LATITUDES_DEG_N = numpy.array([6, 9, 12, 20], dtype=float)
+THIRD_CROPPED_LONGITUDES_DEG_E = numpy.array(
+    [15, 25, 35, 45, 55, 65], dtype=float
+)
+
+FOURTH_CROPPED_DATA_MATRIX = numpy.array([
+    [43, 44, 45, 46, 47, 48],
+    [55, 56, 57, 58, 59, 60],
+    [67, 68, 69, 70, 71, 72],
+    [79, 80, 81, 82, 83, 84]
+], dtype=float)
+
+FOURTH_CROPPED_LATITUDES_DEG_N = numpy.array([-4, -2, 0, 3], dtype=float)
+FOURTH_CROPPED_LONGITUDES_DEG_E = numpy.array(
+    [25, 35, 45, 55, 65, 75], dtype=float
+)
+
+FIFTH_CROPPED_DATA_MATRIX = numpy.array([
+    [98, 99, 100, 101, 102, 103],
+    [110, 111, 112, 113, 114, 115],
+    [122, 123, 124, 125, 126, 127],
+    [122, 123, 124, 125, 126, 127]
+], dtype=float)
+
+FIFTH_CROPPED_LATITUDES_DEG_N = numpy.array([9, 12, 20, 28], dtype=float)
+FIFTH_CROPPED_LONGITUDES_DEG_E = numpy.array(
+    [355, 0, 5, 10, 15, 25], dtype=float
+)
+
+# The following constants are used to test get_cyclone_id and parse_cyclone_id.
 YEAR = 1998
 BASIN_ID_STRING = 'AL'
 CYCLONE_NUMBER = 5
@@ -11,6 +126,228 @@ CYCLONE_ID_STRING = '1998AL05'
 
 class SatelliteUtilsTests(unittest.TestCase):
     """Each method is a unit test for satellite_utils.py."""
+
+    def test_find_storm_center_px_space_first(self):
+        """Ensures correct output from _find_storm_center_px_space.
+
+        In this case, using first storm center.
+        """
+
+        this_row, this_column = satellite_utils._find_storm_center_px_space(
+            storm_latitude_deg_n=FIRST_STORM_LATITUDE_DEG_N,
+            storm_longitude_deg_e=FIRST_STORM_LONGITUDE_DEG_E,
+            grid_latitudes_deg_n=GRID_LATITUDES_DEG_N + 0.,
+            grid_longitudes_deg_e=GRID_LONGITUDES_DEG_E + 0.
+        )
+
+        self.assertTrue(this_row == FIRST_STORM_ROW)
+        self.assertTrue(this_column == FIRST_STORM_COLUMN)
+
+    def test_find_storm_center_px_space_second(self):
+        """Ensures correct output from _find_storm_center_px_space.
+
+        In this case, using second storm center.
+        """
+
+        this_row, this_column = satellite_utils._find_storm_center_px_space(
+            storm_latitude_deg_n=SECOND_STORM_LATITUDE_DEG_N,
+            storm_longitude_deg_e=SECOND_STORM_LONGITUDE_DEG_E,
+            grid_latitudes_deg_n=GRID_LATITUDES_DEG_N + 0.,
+            grid_longitudes_deg_e=GRID_LONGITUDES_DEG_E + 0.
+        )
+
+        self.assertTrue(this_row == SECOND_STORM_ROW)
+        self.assertTrue(this_column == SECOND_STORM_COLUMN)
+
+    def test_find_storm_center_px_space_third(self):
+        """Ensures correct output from _find_storm_center_px_space.
+
+        In this case, using third storm center.
+        """
+
+        this_row, this_column = satellite_utils._find_storm_center_px_space(
+            storm_latitude_deg_n=THIRD_STORM_LATITUDE_DEG_N,
+            storm_longitude_deg_e=THIRD_STORM_LONGITUDE_DEG_E,
+            grid_latitudes_deg_n=GRID_LATITUDES_DEG_N + 0.,
+            grid_longitudes_deg_e=GRID_LONGITUDES_DEG_E + 0.
+        )
+
+        self.assertTrue(this_row == THIRD_STORM_ROW)
+        self.assertTrue(this_column == THIRD_STORM_COLUMN)
+
+    def test_find_storm_center_px_space_fourth(self):
+        """Ensures correct output from _find_storm_center_px_space.
+
+        In this case, using fourth storm center.
+        """
+
+        this_row, this_column = satellite_utils._find_storm_center_px_space(
+            storm_latitude_deg_n=FOURTH_STORM_LATITUDE_DEG_N,
+            storm_longitude_deg_e=FOURTH_STORM_LONGITUDE_DEG_E,
+            grid_latitudes_deg_n=GRID_LATITUDES_DEG_N + 0.,
+            grid_longitudes_deg_e=GRID_LONGITUDES_DEG_E + 0.
+        )
+
+        self.assertTrue(this_row == FOURTH_STORM_ROW)
+        self.assertTrue(this_column == FOURTH_STORM_COLUMN)
+
+    def test_find_storm_center_px_space_fifth(self):
+        """Ensures correct output from _find_storm_center_px_space.
+
+        In this case, using fifth storm center.
+        """
+
+        this_row, this_column = satellite_utils._find_storm_center_px_space(
+            storm_latitude_deg_n=FIFTH_STORM_LATITUDE_DEG_N,
+            storm_longitude_deg_e=FIFTH_STORM_LONGITUDE_DEG_E,
+            grid_latitudes_deg_n=GRID_LATITUDES_DEG_N + 0.,
+            grid_longitudes_deg_e=GRID_LONGITUDES_DEG_E + 0.
+        )
+
+        self.assertTrue(this_row == FIFTH_STORM_ROW)
+        self.assertTrue(this_column == FIFTH_STORM_COLUMN)
+
+    def test_crop_image_around_storm_center_first(self):
+        """Ensures correct output from _crop_image_around_storm_center.
+
+        In this case, using first storm center.
+        """
+
+        (
+            this_data_matrix, these_latitudes_deg_n, these_longitudes_deg_e
+        ) = satellite_utils._crop_image_around_storm_center(
+            data_matrix=UNCROPPED_DATA_MATRIX + 0.,
+            grid_latitudes_deg_n=GRID_LATITUDES_DEG_N + 0.,
+            grid_longitudes_deg_e=GRID_LONGITUDES_DEG_E + 0.,
+            storm_row=FIRST_STORM_ROW, storm_column=FIRST_STORM_COLUMN,
+            num_cropped_rows=NUM_CROPPED_ROWS,
+            num_cropped_columns=NUM_CROPPED_COLUMNS
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_data_matrix, FIRST_CROPPED_DATA_MATRIX, atol=TOLERANCE
+        ))
+        self.assertTrue(numpy.allclose(
+            these_latitudes_deg_n, FIRST_CROPPED_LATITUDES_DEG_N, atol=TOLERANCE
+        ))
+        self.assertTrue(numpy.allclose(
+            these_longitudes_deg_e, FIRST_CROPPED_LONGITUDES_DEG_E,
+            atol=TOLERANCE
+        ))
+
+    def test_crop_image_around_storm_center_second(self):
+        """Ensures correct output from _crop_image_around_storm_center.
+
+        In this case, using second storm center.
+        """
+
+        (
+            this_data_matrix, these_latitudes_deg_n, these_longitudes_deg_e
+        ) = satellite_utils._crop_image_around_storm_center(
+            data_matrix=UNCROPPED_DATA_MATRIX + 0.,
+            grid_latitudes_deg_n=GRID_LATITUDES_DEG_N + 0.,
+            grid_longitudes_deg_e=GRID_LONGITUDES_DEG_E + 0.,
+            storm_row=SECOND_STORM_ROW, storm_column=SECOND_STORM_COLUMN,
+            num_cropped_rows=NUM_CROPPED_ROWS,
+            num_cropped_columns=NUM_CROPPED_COLUMNS
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_data_matrix, SECOND_CROPPED_DATA_MATRIX, atol=TOLERANCE
+        ))
+        self.assertTrue(numpy.allclose(
+            these_latitudes_deg_n, SECOND_CROPPED_LATITUDES_DEG_N,
+            atol=TOLERANCE
+        ))
+        self.assertTrue(numpy.allclose(
+            these_longitudes_deg_e, SECOND_CROPPED_LONGITUDES_DEG_E,
+            atol=TOLERANCE
+        ))
+
+    def test_crop_image_around_storm_center_third(self):
+        """Ensures correct output from _crop_image_around_storm_center.
+
+        In this case, using third storm center.
+        """
+
+        (
+            this_data_matrix, these_latitudes_deg_n, these_longitudes_deg_e
+        ) = satellite_utils._crop_image_around_storm_center(
+            data_matrix=UNCROPPED_DATA_MATRIX + 0.,
+            grid_latitudes_deg_n=GRID_LATITUDES_DEG_N + 0.,
+            grid_longitudes_deg_e=GRID_LONGITUDES_DEG_E + 0.,
+            storm_row=THIRD_STORM_ROW, storm_column=THIRD_STORM_COLUMN,
+            num_cropped_rows=NUM_CROPPED_ROWS,
+            num_cropped_columns=NUM_CROPPED_COLUMNS
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_data_matrix, THIRD_CROPPED_DATA_MATRIX, atol=TOLERANCE
+        ))
+        self.assertTrue(numpy.allclose(
+            these_latitudes_deg_n, THIRD_CROPPED_LATITUDES_DEG_N, atol=TOLERANCE
+        ))
+        self.assertTrue(numpy.allclose(
+            these_longitudes_deg_e, THIRD_CROPPED_LONGITUDES_DEG_E,
+            atol=TOLERANCE
+        ))
+
+    def test_crop_image_around_storm_center_fourth(self):
+        """Ensures correct output from _crop_image_around_storm_center.
+
+        In this case, using fourth storm center.
+        """
+
+        (
+            this_data_matrix, these_latitudes_deg_n, these_longitudes_deg_e
+        ) = satellite_utils._crop_image_around_storm_center(
+            data_matrix=UNCROPPED_DATA_MATRIX + 0.,
+            grid_latitudes_deg_n=GRID_LATITUDES_DEG_N + 0.,
+            grid_longitudes_deg_e=GRID_LONGITUDES_DEG_E + 0.,
+            storm_row=FOURTH_STORM_ROW, storm_column=FOURTH_STORM_COLUMN,
+            num_cropped_rows=NUM_CROPPED_ROWS,
+            num_cropped_columns=NUM_CROPPED_COLUMNS
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_data_matrix, FOURTH_CROPPED_DATA_MATRIX, atol=TOLERANCE
+        ))
+        self.assertTrue(numpy.allclose(
+            these_latitudes_deg_n, FOURTH_CROPPED_LATITUDES_DEG_N,
+            atol=TOLERANCE
+        ))
+        self.assertTrue(numpy.allclose(
+            these_longitudes_deg_e, FOURTH_CROPPED_LONGITUDES_DEG_E,
+            atol=TOLERANCE
+        ))
+
+    def test_crop_image_around_storm_center_fifth(self):
+        """Ensures correct output from _crop_image_around_storm_center.
+
+        In this case, using fifth storm center.
+        """
+
+        (
+            this_data_matrix, these_latitudes_deg_n, these_longitudes_deg_e
+        ) = satellite_utils._crop_image_around_storm_center(
+            data_matrix=UNCROPPED_DATA_MATRIX + 0.,
+            grid_latitudes_deg_n=GRID_LATITUDES_DEG_N + 0.,
+            grid_longitudes_deg_e=GRID_LONGITUDES_DEG_E + 0.,
+            storm_row=FIFTH_STORM_ROW, storm_column=FIFTH_STORM_COLUMN,
+            num_cropped_rows=NUM_CROPPED_ROWS,
+            num_cropped_columns=NUM_CROPPED_COLUMNS
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_data_matrix, FIFTH_CROPPED_DATA_MATRIX, atol=TOLERANCE
+        ))
+        self.assertTrue(numpy.allclose(
+            these_latitudes_deg_n, FIFTH_CROPPED_LATITUDES_DEG_N, atol=TOLERANCE
+        ))
+        self.assertTrue(numpy.allclose(
+            these_longitudes_deg_e, FIFTH_CROPPED_LONGITUDES_DEG_E,
+            atol=TOLERANCE
+        ))
 
     def test_get_cyclone_id(self):
         """Ensures correct output from get_cyclone_id."""
