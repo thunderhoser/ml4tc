b6e6a00cf361f7e120227378fa95b7ebdad1fd8e
thunderhoser <lagerqui@ualberta.ca>
2022-02-16 20:54:13 -0700

    FOO

--- ml4tc/scalar_satellite_plotting.py
+++ ml4tc/scalar_satellite_plotting.py
@@ -35,9 +35,9 @@ FIGURE_RESOLUTION_DPI = 300
 FIGURE_WIDTH_INCHES = 15
 FIGURE_HEIGHT_INCHES = 15
 
-DEFAULT_FONT_SIZE = 20
-VALID_TIME_FONT_SIZE = 12
-PREDICTOR_FONT_SIZE = 12
+DEFAULT_FONT_SIZE = 30
+DEFAULT_TIME_TICK_FONT_SIZE = 30
+DEFAULT_PREDICTOR_TICK_FONT_SIZE = 20
 
 pyplot.rc('font', size=DEFAULT_FONT_SIZE)
 pyplot.rc('axes', titlesize=DEFAULT_FONT_SIZE)
@@ -138,13 +138,17 @@ def plot_bar_graph_one_time(
 
 
 def plot_colour_map_multi_times(
-        example_table_xarray, time_indices, predictor_indices, info_string=None,
-        figure_object=None, axes_object=None):
+        example_table_xarray, time_indices, predictor_indices,
+        time_tick_font_size=DEFAULT_TIME_TICK_FONT_SIZE,
+        predictor_tick_font_size=DEFAULT_PREDICTOR_TICK_FONT_SIZE,
+        info_string=None, figure_object=None, axes_object=None):
     """Plots predictors at many times as colour map.
 
     :param example_table_xarray: See doc for `plot_bar_graph_one_time`.
     :param time_indices: 1-D numpy array with indices of valid times to plot.
     :param predictor_indices: See doc for `plot_bar_graph_one_time`.
+    :param time_tick_font_size: Font size for tick labels on y-axis.
+    :param predictor_tick_font_size: Font size for tick labels on x-axis.
     :param info_string: Same.
     :param figure_object: Same.
     :param axes_object: Same.
@@ -201,9 +205,7 @@ def plot_colour_map_multi_times(
         dtype=float
     )
     y_tick_labels = ['{0:s}'.format(t) for t in valid_time_strings]
-    pyplot.yticks(
-        y_tick_values, y_tick_labels, fontsize=VALID_TIME_FONT_SIZE
-    )
+    pyplot.yticks(y_tick_values, y_tick_labels, fontsize=time_tick_font_size)
     axes_object.set_ylabel('Valid time')
 
     x_tick_values = numpy.linspace(
@@ -215,7 +217,8 @@ def plot_colour_map_multi_times(
     ].values[predictor_indices].tolist()
 
     pyplot.xticks(
-        x_tick_values, x_tick_labels, rotation=90., fontsize=PREDICTOR_FONT_SIZE
+        x_tick_values, x_tick_labels, rotation=90.,
+        fontsize=predictor_tick_font_size
     )
 
     title_string = 'Satellite for {0:s}'.format(cyclone_id_string)
@@ -298,7 +301,8 @@ def plot_pm_signs_multi_times(
 
 def plot_raw_numbers_multi_times(
         data_matrix, axes_object, font_size, colour_map_object,
-        min_colour_value, max_colour_value, number_format_string):
+        min_colour_value, max_colour_value, number_format_string,
+        plot_in_log_space=False):
     """Plots data at many times with raw numbers (text).
 
     This method is good for plotting saliency, input * gradient, or any other
@@ -317,12 +321,15 @@ def plot_raw_numbers_multi_times(
     :param max_colour_value: Max value in colour scheme.
     :param number_format_string: Format for printing numbers.  Valid examples
         are ".2f", "d", etc.
+    :param plot_in_log_space: Boolean flag.  If True (False), colours will be
+        scaled logarithmically (linearly).
     """
 
     error_checking.assert_is_numpy_array_without_nan(data_matrix)
     error_checking.assert_is_numpy_array(data_matrix, num_dimensions=2)
     error_checking.assert_is_greater(max_colour_value, min_colour_value)
     error_checking.assert_is_string(number_format_string)
+    error_checking.assert_is_boolean(plot_in_log_space)
 
     number_format_string = '{0:' + number_format_string + '}'
 
@@ -340,16 +347,25 @@ def plot_raw_numbers_multi_times(
     colour_norm_object = pyplot.Normalize(
         vmin=min_colour_value, vmax=max_colour_value
     )
-    rgb_matrix = colour_map_object(colour_norm_object(
-        numpy.absolute(data_matrix)
-    ))[..., :-1]
+
+    if plot_in_log_space:
+        rgb_matrix = colour_map_object(colour_norm_object(
+            numpy.log10(1 + numpy.absolute(data_matrix))
+        ))[..., :-1]
+    else:
+        rgb_matrix = colour_map_object(colour_norm_object(
+            numpy.absolute(data_matrix)
+        ))[..., :-1]
 
     for i in range(num_grid_rows):
         for j in range(num_grid_columns):
             axes_object.text(
                 x_coords[j], y_coords[i],
                 number_format_string.format(data_matrix[i, j]),
-                fontsize=font_size, color=rgb_matrix[i, j, ...],
+                fontsize=font_size,
+                fontstyle='italic' if data_matrix[i, j] < 0 else 'normal',
+                fontweight='bold' if data_matrix[i, j] > 0 else 'normal',
+                color=rgb_matrix[i, j, ...],
                 horizontalalignment='center', verticalalignment='center',
                 transform=axes_object.transAxes
             )
