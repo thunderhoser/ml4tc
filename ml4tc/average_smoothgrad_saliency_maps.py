81bf4046522e888cc8047152dd4bef4c8077f039
thunderhoser <lagerqui@ualberta.ca>
2022-03-10 18:40:09 -0700

    FOO

--- ml4tc/average_smoothgrad_saliency_maps.py
+++ ml4tc/average_smoothgrad_saliency_maps.py
@@ -97,6 +97,9 @@ def _run(input_file_pattern, num_smoothgrad_samples, output_file_name):
                 ]
             )
 
+        print(new_saliency_dict[saliency.NEURON_INDICES_KEY])
+        print('\n\n\n\n************\n\n\n\n')
+
         if not bool(saliency_dict):
             saliency_dict = copy.deepcopy(new_saliency_dict)
 
