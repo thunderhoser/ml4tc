8179a1ed9e071420890936392df5021f87999a37
thunderhoser <lagerqui@ualberta.ca>
2022-06-03 16:00:03 -0600

    FOO

--- ml4tc/neural_net.py
+++ ml4tc/neural_net.py
@@ -2366,10 +2366,12 @@ def input_generator(option_dict):
     cyclone_id_strings = [cyclone_id_strings[k] for k in good_indices]
     random.shuffle(cyclone_id_strings)
 
+    # TODO(thunderhoser): Do not allow zipped!
+
     example_file_names = [
         example_io.find_file(
             directory_name=example_dir_name, cyclone_id_string=c,
-            prefer_zipped=False, allow_other_format=False,
+            prefer_zipped=False, allow_other_format=True,
             raise_error_if_missing=True
         )
         for c in cyclone_id_strings
