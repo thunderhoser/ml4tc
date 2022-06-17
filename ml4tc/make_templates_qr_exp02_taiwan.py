89e1eb4d0f109f8e4ed7b300be291f9f1c5606ad
thunderhoser <lagerqui@ualberta.ca>
2022-06-17 11:25:30 -0600

    FOO

--- ml4tc/make_templates_qr_exp02_taiwan.py
+++ ml4tc/make_templates_qr_exp02_taiwan.py
@@ -73,26 +73,12 @@ CENTRAL_LOSS_FUNCTIONS = [
     for w in CENTRAL_LOSS_WEIGHTS
 ]
 
-QUANTILE_LEVEL_SETS = [
-    numpy.linspace(0.01, 0.99, num=99),
-    numpy.linspace(0.01, 0.99, num=99)[::2],
-    numpy.linspace(0.01, 0.99, num=99)[::3],
-    numpy.linspace(0.01, 0.99, num=99)[::4],
-    numpy.linspace(0.01, 0.99, num=99)[::5],
-    numpy.linspace(0.01, 0.99, num=99)[::6],
-    numpy.linspace(0.01, 0.99, num=99)[::7],
-    numpy.linspace(0.01, 0.99, num=99)[::8],
-    numpy.linspace(0.01, 0.99, num=99)[::9]
-]
+QUANTILE_LEVELS = numpy.array([
+    0.005, 0.995, 0.025, 0.975, 0.05, 0.95, 0.1, 0.9, 0.25, 0.5, 0.75,
+    0.0125, 0.9875, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8
+])
 
-QUANTILE_LEVEL_SETS = [
-    numpy.concatenate((
-        numpy.array([0.025, 0.25, 0.5, 0.75, 0.975, 0.99]), s
-    ))
-    for s in QUANTILE_LEVEL_SETS
-]
-
-QUANTILE_LEVEL_SETS = [numpy.sort(numpy.unique(s)) for s in QUANTILE_LEVEL_SETS]
+QUANTILE_LEVELS = numpy.sort(QUANTILE_LEVELS)
 
 OUTPUT_DIR_NAME = (
     '/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tc_models/'
@@ -111,86 +97,80 @@ def _run():
     )
 
     for i in range(len(CENTRAL_LOSS_WEIGHTS)):
-        for j in range(len(QUANTILE_LEVEL_SETS)):
-            option_dict_ships = copy.deepcopy(BASE_OPTION_DICT_SHIPS)
-            option_dict_dense = copy.deepcopy(BASE_OPTION_DICT_DENSE)
+        option_dict_ships = copy.deepcopy(BASE_OPTION_DICT_SHIPS)
+        option_dict_dense = copy.deepcopy(BASE_OPTION_DICT_DENSE)
 
-            d = BASE_OPTION_DICT_GRIDDED_SAT
-            num_flattened_features = (
-                option_dict_ships[cnn_architecture.NUM_NEURONS_KEY][-1] +
-                40 * d[cnn_architecture.NUM_CHANNELS_KEY][-1]
-            )
+        d = BASE_OPTION_DICT_GRIDDED_SAT
+        num_flattened_features = (
+            option_dict_ships[cnn_architecture.NUM_NEURONS_KEY][-1] +
+            40 * d[cnn_architecture.NUM_CHANNELS_KEY][-1]
+        )
 
-            exp_neuron_counts = architecture_utils.get_dense_layer_dimensions(
-                num_input_units=num_flattened_features,
-                num_classes=2, num_dense_layers=5,
-                for_classification=True
-            )[1]
+        exp_neuron_counts = architecture_utils.get_dense_layer_dimensions(
+            num_input_units=num_flattened_features,
+            num_classes=2, num_dense_layers=5,
+            for_classification=True
+        )[1]
 
-            neuron_counts = numpy.linspace(
-                1, exp_neuron_counts[1], num=4, dtype=int
-            )[::-1]
+        neuron_counts = numpy.linspace(
+            1, exp_neuron_counts[1], num=4, dtype=int
+        )[::-1]
 
-            neuron_counts = numpy.concatenate((
-                exp_neuron_counts[[0]], neuron_counts
-            ), axis=0)
+        neuron_counts = numpy.concatenate((
+            exp_neuron_counts[[0]], neuron_counts
+        ), axis=0)
 
-            # option_dict_dense[cnn_architecture.NUM_NEURONS_KEY] = neuron_counts
-            option_dict_dense[cnn_architecture.NUM_NEURONS_KEY] = (
-                exp_neuron_counts
-            )
+        option_dict_dense[cnn_architecture.NUM_NEURONS_KEY] = neuron_counts
 
-            model_object = cnn_architecture.create_qr_model_td_to_ts(
-                option_dict_gridded_sat=BASE_OPTION_DICT_GRIDDED_SAT,
-                option_dict_ungridded_sat=None,
-                option_dict_ships=option_dict_ships,
-                option_dict_dense=option_dict_dense,
-                central_loss_function=CENTRAL_LOSS_FUNCTIONS[i],
-                quantile_levels=QUANTILE_LEVEL_SETS[j],
-                num_lead_times=len(LEAD_TIMES_HOURS)
-            )
+        model_object = cnn_architecture.create_qr_model_td_to_ts(
+            option_dict_gridded_sat=BASE_OPTION_DICT_GRIDDED_SAT,
+            option_dict_ungridded_sat=None,
+            option_dict_ships=option_dict_ships,
+            option_dict_dense=option_dict_dense,
+            central_loss_function=CENTRAL_LOSS_FUNCTIONS[i],
+            quantile_levels=QUANTILE_LEVELS,
+            num_lead_times=len(LEAD_TIMES_HOURS)
+        )
 
-            model_file_name = (
-                '{0:s}/central-loss-weight={1:04.1f}_'
-                'num-quantile-levels={2:03d}/model.h5'
-            ).format(
-                OUTPUT_DIR_NAME, CENTRAL_LOSS_WEIGHTS[i],
-                len(QUANTILE_LEVEL_SETS[j])
-            )
+        model_file_name = (
+            '{0:s}/central-loss-weight={1:04.1f}/model.h5'
+        ).format(
+            OUTPUT_DIR_NAME, CENTRAL_LOSS_WEIGHTS[i]
+        )
 
-            file_system_utils.mkdir_recursive_if_necessary(
-                file_name=model_file_name
-            )
+        file_system_utils.mkdir_recursive_if_necessary(
+            file_name=model_file_name
+        )
 
-            print('Writing model to: "{0:s}"...'.format(model_file_name))
-            model_object.save(
-                filepath=model_file_name, overwrite=True,
-                include_optimizer=True
-            )
+        print('Writing model to: "{0:s}"...'.format(model_file_name))
+        model_object.save(
+            filepath=model_file_name, overwrite=True,
+            include_optimizer=True
+        )
 
-            model_metafile_name = neural_net.find_metafile(
-                model_file_name=model_file_name,
-                raise_error_if_missing=False
-            )
+        model_metafile_name = neural_net.find_metafile(
+            model_file_name=model_file_name,
+            raise_error_if_missing=False
+        )
 
-            dummy_option_dict = neural_net.DEFAULT_GENERATOR_OPTION_DICT
-            dummy_option_dict[neural_net.PREDICT_TD_TO_TS_KEY] = True
-            dummy_option_dict[neural_net.LEAD_TIMES_KEY] = LEAD_TIMES_HOURS
-            dummy_option_dict[neural_net.SATELLITE_LAG_TIMES_KEY] = None
+        dummy_option_dict = neural_net.DEFAULT_GENERATOR_OPTION_DICT
+        dummy_option_dict[neural_net.PREDICT_TD_TO_TS_KEY] = True
+        dummy_option_dict[neural_net.LEAD_TIMES_KEY] = LEAD_TIMES_HOURS
+        dummy_option_dict[neural_net.SATELLITE_LAG_TIMES_KEY] = None
 
-            print('Writing metadata to: "{0:s}"...'.format(
-                model_metafile_name
-            ))
-            neural_net._write_metafile(
-                pickle_file_name=model_metafile_name, num_epochs=100,
-                quantile_levels=QUANTILE_LEVEL_SETS[j],
-                central_loss_function_weight=CENTRAL_LOSS_WEIGHTS[i],
-                num_training_batches_per_epoch=100,
-                training_option_dict=dummy_option_dict,
-                num_validation_batches_per_epoch=100,
-                validation_option_dict=dummy_option_dict,
-                do_early_stopping=True, plateau_lr_multiplier=0.6
-            )
+        print('Writing metadata to: "{0:s}"...'.format(
+            model_metafile_name
+        ))
+        neural_net._write_metafile(
+            pickle_file_name=model_metafile_name, num_epochs=100,
+            quantile_levels=QUANTILE_LEVELS,
+            central_loss_function_weight=CENTRAL_LOSS_WEIGHTS[i],
+            num_training_batches_per_epoch=100,
+            training_option_dict=dummy_option_dict,
+            num_validation_batches_per_epoch=100,
+            validation_option_dict=dummy_option_dict,
+            do_early_stopping=True, plateau_lr_multiplier=0.6
+        )
 
 
 if __name__ == '__main__':
