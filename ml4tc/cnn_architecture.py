c8073931a0d5627c1846ac3524a7f8de43e31297
thunderhoser <lagerqui@ualberta.ca>
2022-07-08 18:25:45 -0600

    FOO

--- ml4tc/cnn_architecture.py
+++ ml4tc/cnn_architecture.py
@@ -103,9 +103,11 @@ def _abs_value_for_diffs_function():
     return abs_value_function
 
 
-def _relu_for_diffs_function():
+def _relu_for_diffs_function(over_lead_times_only=False):
     """Returns function that takes applies ReLU to differences only.
 
+    :param over_lead_times_only: Boolean flag.  If 1, will apply to lead-time
+        axis only, not prediction-set axis.
     :return: relu_function: Function handle (see below).
     """
 
@@ -116,10 +118,11 @@ def _relu_for_diffs_function():
         :return: output_tensor_3d: Same but after applying ReLU to differences.
         """
 
-        output_tensor_3d = K.concatenate((
-            input_tensor_3d[..., :1, :2],
-            K.relu(input_tensor_3d[..., :1, 2:])
-        ), axis=-1)
+        if not over_lead_times_only:
+            output_tensor_3d = K.concatenate((
+                input_tensor_3d[..., :1, :2],
+                K.relu(input_tensor_3d[..., :1, 2:])
+            ), axis=-1)
 
         return K.concatenate((
             output_tensor_3d,
@@ -1189,7 +1192,8 @@ def create_qr_model_td_to_ts(
     # )(output_layer_object)
 
     output_layer_object = keras.layers.Lambda(
-        _relu_for_diffs_function(), name='actual_relu_for_differences'
+        _relu_for_diffs_function(over_lead_times_only=False),
+        name='actual_relu_for_differences'
     )(output_layer_object)
 
     this_function = _cumulative_sum_function(over_lead_times=True)
@@ -1367,7 +1371,8 @@ def create_basic_model_td_to_ts(
     # )(output_layer_object)
 
     output_layer_object = keras.layers.Lambda(
-        _relu_for_diffs_function(), name='actual_relu_for_differences'
+        _relu_for_diffs_function(over_lead_times_only=False),
+        name='actual_relu_for_differences'
     )(output_layer_object)
 
     this_function = _cumulative_sum_function(over_lead_times=True)
@@ -1386,6 +1391,185 @@ def create_basic_model_td_to_ts(
     )
     model_object.compile(
         loss=keras.losses.binary_crossentropy, optimizer=keras.optimizers.Adam()
+    )
+    model_object.summary()
+
+    return model_object
+
+
+def create_crps_model_td_to_ts(
+        option_dict_gridded_sat, option_dict_ungridded_sat, option_dict_ships,
+        option_dict_dense, num_lead_times, num_estimates):
+    """Creates CNN with CRPS loss function for TD-to-TS prediction.
+
+    :param option_dict_gridded_sat: See doc for `create_model`.
+    :param option_dict_ungridded_sat: Same.
+    :param option_dict_ships: Same.
+    :param option_dict_dense: Same.
+    :param num_lead_times: Number of lead times.
+    :param num_estimates: Number of estimates in ensemble.
+    :return: model_object: Untrained CNN (instance of `keras.models.Model`).
+    """
+
+    error_checking.assert_is_integer(num_lead_times)
+    error_checking.assert_is_greater(num_lead_times, 0)
+    error_checking.assert_is_integer(num_estimates)
+    error_checking.assert_is_greater(num_estimates, 1)
+
+    input_layer_objects = []
+    flattening_layer_objects = []
+
+    if option_dict_gridded_sat is not None:
+        option_dict_gridded_sat_orig = option_dict_gridded_sat.copy()
+        option_dict_gridded_sat = DEFAULT_OPTION_DICT_GRIDDED_SAT.copy()
+        option_dict_gridded_sat.update(option_dict_gridded_sat_orig)
+
+        option_dict_gridded_sat[DROPOUT_MC_FLAGS_KEY][:] = False
+
+        this_input_layer_object, this_flattening_layer_object = (
+            _create_layers_gridded_sat(option_dict_gridded_sat)
+        )
+
+        input_layer_objects.append(this_input_layer_object)
+        flattening_layer_objects.append(this_flattening_layer_object)
+
+    if option_dict_ungridded_sat is not None:
+        option_dict_ungridded_sat_orig = option_dict_ungridded_sat.copy()
+        option_dict_ungridded_sat = DEFAULT_OPTION_DICT_UNGRIDDED_SAT.copy()
+        option_dict_ungridded_sat.update(option_dict_ungridded_sat_orig)
+
+        option_dict_ungridded_sat[DROPOUT_MC_FLAGS_KEY][:] = False
+
+        input_dimensions = option_dict_ungridded_sat[INPUT_DIMENSIONS_KEY]
+        this_input_layer_object = keras.layers.Input(
+            shape=tuple(input_dimensions.tolist())
+        )
+        new_dimensions = (numpy.prod(input_dimensions),)
+        this_layer_object = keras.layers.Reshape(target_shape=new_dimensions)(
+            this_input_layer_object
+        )
+
+        this_flattening_layer_object = create_dense_layers(
+            input_layer_object=this_layer_object,
+            option_dict=option_dict_ungridded_sat
+        )
+
+        if option_dict_ungridded_sat[USE_BATCH_NORM_KEY]:
+            this_flattening_layer_object = (
+                architecture_utils.get_batch_norm_layer()(
+                    this_flattening_layer_object
+                )
+            )
+
+        input_layer_objects.append(this_input_layer_object)
+        flattening_layer_objects.append(this_flattening_layer_object)
+
+    if option_dict_ships is not None:
+        option_dict_ships_orig = option_dict_ships.copy()
+        option_dict_ships = DEFAULT_OPTION_DICT_SHIPS.copy()
+        option_dict_ships.update(option_dict_ships_orig)
+
+        option_dict_ships[DROPOUT_MC_FLAGS_KEY][:] = False
+
+        input_dimensions = option_dict_ships[INPUT_DIMENSIONS_KEY]
+        this_input_layer_object = keras.layers.Input(
+            shape=tuple(input_dimensions.tolist())
+        )
+        new_dimensions = (numpy.prod(input_dimensions),)
+        this_layer_object = keras.layers.Reshape(target_shape=new_dimensions)(
+            this_input_layer_object
+        )
+
+        this_flattening_layer_object = create_dense_layers(
+            input_layer_object=this_layer_object,
+            option_dict=option_dict_ships
+        )
+
+        if option_dict_ships[USE_BATCH_NORM_KEY]:
+            this_flattening_layer_object = (
+                architecture_utils.get_batch_norm_layer()(
+                    this_flattening_layer_object
+                )
+            )
+
+        input_layer_objects.append(this_input_layer_object)
+        flattening_layer_objects.append(this_flattening_layer_object)
+
+    option_dict_dense_orig = option_dict_dense.copy()
+    option_dict_dense = DEFAULT_OPTION_DICT_DENSE.copy()
+    option_dict_dense.update(option_dict_dense_orig)
+
+    # num_output_neurons = option_dict_dense[NUM_NEURONS_KEY][-1] + 0
+    output_activ_function_name = copy.deepcopy(
+        option_dict_dense[OUTPUT_ACTIV_FUNCTION_KEY]
+    )
+    output_activ_function_alpha = copy.deepcopy(
+        option_dict_dense[OUTPUT_ACTIV_FUNCTION_ALPHA_KEY]
+    )
+
+    option_dict_dense[NUM_NEURONS_KEY] = (
+        option_dict_dense[NUM_NEURONS_KEY][:-1]
+    )
+    option_dict_dense[DROPOUT_RATES_KEY] = (
+        option_dict_dense[DROPOUT_RATES_KEY][:-1]
+    )
+    option_dict_dense[DROPOUT_MC_FLAGS_KEY] = (
+        option_dict_dense[DROPOUT_MC_FLAGS_KEY][:-1]
+    )
+    option_dict_dense[OUTPUT_ACTIV_FUNCTION_KEY] = (
+        option_dict_dense[INNER_ACTIV_FUNCTION_KEY]
+    )
+    option_dict_dense[OUTPUT_ACTIV_FUNCTION_ALPHA_KEY] = (
+        option_dict_dense[INNER_ACTIV_FUNCTION_ALPHA_KEY]
+    )
+
+    option_dict_dense[DROPOUT_MC_FLAGS_KEY][:] = False
+
+    if len(flattening_layer_objects) > 1:
+        current_layer_object = keras.layers.concatenate(
+            flattening_layer_objects
+        )
+    else:
+        current_layer_object = flattening_layer_objects[0]
+
+    current_layer_object = create_dense_layers(
+        input_layer_object=current_layer_object, option_dict=option_dict_dense
+    )
+
+    regularization_func = architecture_utils.get_weight_regularizer(
+        l2_weight=option_dict_dense[L2_WEIGHT_KEY]
+    )
+
+    output_layer_object = architecture_utils.get_dense_layer(
+        num_output_units=num_lead_times * num_estimates,
+        weight_regularizer=regularization_func
+    )(current_layer_object)
+
+    output_layer_object = keras.layers.Reshape(
+        target_shape=(num_lead_times, num_estimates)
+    )(output_layer_object)
+
+    output_layer_object = keras.layers.Lambda(
+        _relu_for_diffs_function(over_lead_times_only=True),
+        name='actual_relu_for_differences'
+    )(output_layer_object)
+
+    this_function = _cumulative_sum_function(over_lead_times=True)
+    output_layer_object = keras.layers.Lambda(
+        this_function, name='sum_over_lead_times'
+    )(output_layer_object)
+
+    output_layer_object = architecture_utils.get_activation_layer(
+        activation_function_string=output_activ_function_name,
+        alpha_for_relu=output_activ_function_alpha,
+        alpha_for_elu=output_activ_function_alpha
+    )(output_layer_object)
+
+    model_object = keras.models.Model(
+        inputs=input_layer_objects, outputs=output_layer_object
+    )
+    model_object.compile(
+        loss=custom_losses.crps_loss(), optimizer=keras.optimizers.Adam()
     )
     model_object.summary()
 
