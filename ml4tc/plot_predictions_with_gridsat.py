d952bdc05c1d912a601c53d1b8700f86492f2a02
thunderhoser <lagerqui@ualberta.ca>
2022-11-30 15:36:38 -0700

    FOO

--- ml4tc/plot_predictions_with_gridsat.py
+++ ml4tc/plot_predictions_with_gridsat.py
@@ -287,9 +287,10 @@ def _plot_predictions_and_targets(
         prediction_io.PROBABILITY_MATRIX_KEY
     ][example_indices, 1, ...]
 
-    print(prediction_dict[prediction_io.PROBABILITY_MATRIX_KEY][example_indices, 1, ...])
-    print(prediction_dict[prediction_io.PROBABILITY_MATRIX_KEY][example_indices, 1, ...].shape)
-    print(prediction_dict[prediction_io.PROBABILITY_MATRIX_KEY].shape)
+    print(prediction_dict[prediction_io.PROBABILITY_MATRIX_KEY][example_indices, 1, 0, ...])
+    print('\n')
+    print(prediction_dict[prediction_io.PROBABILITY_MATRIX_KEY][example_indices, 1, -1, ...])
+    print('\n\n\n')
 
     lead_times_hours = prediction_dict[prediction_io.LEAD_TIMES_KEY]
     quantile_levels = prediction_dict[prediction_io.QUANTILE_LEVELS_KEY]
