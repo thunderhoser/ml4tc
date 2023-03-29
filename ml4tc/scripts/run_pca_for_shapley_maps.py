1f1cdf21e96821eb1deb5005c023e0eb35b1cb8a
thunderhoser <lagerqui@ualberta.ca>
2023-03-29 13:55:22 -0600

    Saving intermediate shit.

--- ml4tc/scripts/run_pca_for_shapley_maps.py
+++ ml4tc/scripts/run_pca_for_shapley_maps.py
@@ -56,37 +56,29 @@ def _run(shapley_file_names, num_examples_to_keep, output_file_name):
     error_checking.assert_is_geq(num_examples_to_keep, 100)
 
     shapley_matrix = None
-    predictor_matrix = None
+    cyclone_id_strings = []
+    init_times_unix_sec = numpy.array([], dtype=int)
 
     # TODO(thunderhoser): Ensure matching saliency metadata for input files.
     for this_file_name in shapley_file_names:
         print('Reading data from: "{0:s}"...'.format(this_file_name))
         this_saliency_dict = saliency.read_file(this_file_name)
 
-        this_dummy_input_grad_matrix = (
-            this_saliency_dict[saliency.THREE_INPUT_GRAD_KEY][0]
-        )
-        this_dummy_saliency_matrix = (
-            this_saliency_dict[saliency.THREE_SALIENCY_KEY][0]
-        )
-        this_predictor_matrix = numpy.divide(
-            this_dummy_input_grad_matrix, this_dummy_saliency_matrix
+        cyclone_id_strings += this_saliency_dict[saliency.CYCLONE_IDS_KEY]
+        init_times_unix_sec = numpy.concatenate(
+            (init_times_unix_sec, this_saliency_dict[saliency.INIT_TIMES_KEY]),
+            axis=0
         )
 
-        this_predictor_matrix[
-            numpy.invert(numpy.isfinite(this_predictor_matrix))
-        ] = 0.
-        this_shapley_matrix = this_dummy_input_grad_matrix
+        this_shapley_matrix = (
+            this_saliency_dict[saliency.THREE_INPUT_GRAD_KEY][0]
+        )
 
         if shapley_matrix is None:
             shapley_matrix = this_shapley_matrix + 0.
-            predictor_matrix = this_predictor_matrix + 0.
         else:
             shapley_matrix = numpy.concatenate(
                 (shapley_matrix, this_shapley_matrix), axis=0
-            )
-            predictor_matrix = numpy.concatenate(
-                (predictor_matrix, this_predictor_matrix), axis=0
             )
 
     print(SEPARATOR_STRING)
@@ -100,15 +92,13 @@ def _run(shapley_file_names, num_examples_to_keep, output_file_name):
         good_indices = numpy.random.choice(
             all_indices, size=num_examples_to_keep, replace=False
         )
+        num_examples = num_examples_to_keep
 
         shapley_matrix = shapley_matrix[good_indices, ...]
-        predictor_matrix = predictor_matrix[good_indices, ...]
+        cyclone_id_strings = [cyclone_id_strings[k] for k in good_indices]
+        init_times_unix_sec = init_times_unix_sec[good_indices]
 
-    num_examples = shapley_matrix.shape[0]
-
-    mean_shapley_value = numpy.mean(shapley_matrix)
-    stdev_shapley_value = numpy.std(shapley_matrix, ddof=1)
-    shapley_matrix = (shapley_matrix - mean_shapley_value) / stdev_shapley_value
+    dfdsfd
 
     mean_shapley_value = numpy.mean(shapley_matrix)
     stdev_shapley_value = numpy.std(shapley_matrix, ddof=1)
