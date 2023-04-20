894a40716bd027ed0e15cf36e8df193a63aa15b0
thunderhoser <lagerqui@ualberta.ca>
2023-04-20 10:53:40 -0600

    FOO

---
+++ ml4tc/slurm/bnn_experiment02_scaling/validation_sans_uq/apply_model_2005.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+model_dir_name=$1
+
+CODE_DIR_NAME="/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tc_standalone/ml4tc"
+EXAMPLE_DIR_NAME="/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tc_project/learning_examples/rotated_with_storm_motion/imputed/normalized"
+
+echo $model_dir_name
+
+model_file_name="${model_dir_name}/model.h5"
+output_dir_name="${model_dir_name}/validation_2005"
+
+python3 -u "${CODE_DIR_NAME}/apply_neural_net.py" \
+--input_model_file_name="${model_file_name}" \
+--input_example_dir_name="${EXAMPLE_DIR_NAME}" \
+--years 2005 \
+--output_dir_name="${output_dir_name}"
