1b11e9bdde908d0ffeb83e2ca7331976f2951604
thunderhoser <lagerqui@ualberta.ca>
2023-08-09 17:15:43 -0600

    FOO

--- ml4tc/ri2023_plot_case_study.py
+++ ml4tc/ri2023_plot_case_study.py
@@ -19,12 +19,59 @@ sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
 
 import time_conversion
 import file_system_utils
+import gg_plotting_utils
 import prediction_io
+import example_io
+import ships_io
+import satellite_utils
+import general_utils
+import neural_net
+import predictor_plotting
 
 TIME_FORMAT = '%Y-%m-%d-%H'
 
 BASELINE_DESCRIPTION_STRINGS = ['basic', 'consensus', 'dtops']
 BASELINE_DESCRIPTION_STRINGS_FANCY = ['SHIPS-RII', 'SHIPS consensus', 'DTOPS']
+
+SHIPS_LAGGED_PREDICTOR_NAMES = [
+    ships_io.SATELLITE_TEMP_0TO200KM_KEY,
+    ships_io.SATELLITE_TEMP_0TO200KM_STDEV_KEY,
+    ships_io.SATELLITE_TEMP_100TO300KM_KEY,
+    ships_io.SATELLITE_TEMP_100TO300KM_STDEV_KEY,
+    ships_io.SATELLITE_TEMP_FRACTION_BELOW_M10C_KEY,
+    ships_io.SATELLITE_TEMP_FRACTION_BELOW_M20C_KEY,
+    ships_io.SATELLITE_TEMP_FRACTION_BELOW_M30C_KEY,
+    ships_io.SATELLITE_TEMP_FRACTION_BELOW_M40C_KEY,
+    ships_io.SATELLITE_TEMP_FRACTION_BELOW_M50C_KEY,
+    ships_io.SATELLITE_TEMP_FRACTION_BELOW_M60C_KEY,
+    ships_io.SATELLITE_MAX_TEMP_0TO30KM_KEY,
+    ships_io.SATELLITE_MEAN_TEMP_0TO30KM_KEY,
+    ships_io.SATELLITE_MAX_TEMP_RADIUS_KEY,
+    ships_io.SATELLITE_MIN_TEMP_20TO120KM_KEY,
+    ships_io.SATELLITE_MEAN_TEMP_20TO120KM_KEY,
+    ships_io.SATELLITE_MIN_TEMP_RADIUS_KEY
+]
+
+SHIPS_FORECAST_PREDICTOR_NAMES = [
+    ships_io.INTENSITY_KEY,
+    ships_io.INTENSITY_CHANGE_6HOURS_KEY,
+    ships_io.SHEAR_850TO500MB_U_KEY,
+    ships_io.SHEAR_850TO200MB_INNER_RING_GNRL_KEY,
+    ships_io.TEMP_200MB_OUTER_RING_KEY,
+    ships_io.TEMP_GRADIENT_850TO700MB_INNER_RING_KEY,
+    ships_io.MAX_TAN_WIND_850MB_KEY,
+    ships_io.W_WIND_0TO15KM_INNER_RING_KEY,
+    ships_io.FORECAST_LATITUDE_KEY,
+    ships_io.MAX_PTTL_INTENSITY_KEY,
+    ships_io.OCEAN_AGE_KEY,
+    ships_io.MERGED_SST_KEY,
+    ships_io.MERGED_OHC_KEY
+]
+
+SHIPS_MAX_FORECAST_HOUR = 24
+
+KT_TO_METRES_PER_SECOND = 1.852 / 3.6
+RAPID_INTENSIFN_CUTOFF_M_S01 = 30 * KT_TO_METRES_PER_SECOND
 
 BASELINE_COLOUR = numpy.array([27, 158, 119], dtype=float) / 255
 BASELINE_MARKER_TYPE = 'o'
@@ -120,47 +167,25 @@ INPUT_ARG_PARSER.add_argument(
 )
 
 
-def _run(top_nn_model_dir_names, nn_model_description_strings,
-         norm_example_dir_name, normalization_file_name, cyclone_id_string,
-         init_time_string, output_dir_name):
-    """Plots case study with predictors, NN predictions, and baseline predictions.
+def _plot_forecast_probs(
+        top_model_dir_names, model_description_strings, cyclone_id_string,
+        init_time_unix_sec, output_file_name):
+    """Plots forecast probs from all models in one figure.
 
-    This is effectively the main method.
-
-    :param top_nn_model_dir_names: See documentation at top of file.
-    :param nn_model_description_strings: Same.
-    :param norm_example_dir_name: Same.
-    :param normalization_file_name: Same.
+    :param top_model_dir_names: See documentation at top of file.
+    :param model_description_strings: Same.
     :param cyclone_id_string: Same.
-    :param init_time_string: Same.
-    :param output_dir_name: Same.
+    :param init_time_unix_sec: Same.
+    :param output_file_name: Path to output file.  Figure will be saved here.
     """
 
-    init_time_unix_sec = time_conversion.string_to_unix_sec(
-        init_time_string, TIME_FORMAT
-    )
-
-    file_system_utils.mkdir_recursive_if_necessary(
-        directory_name=output_dir_name
-    )
-
-    num_nn_models = len(top_nn_model_dir_names)
+    num_models = len(model_description_strings)
     num_baseline_models = len(BASELINE_DESCRIPTION_STRINGS)
-    num_models = num_nn_models + num_baseline_models
+    num_nn_models = num_models - num_baseline_models
+    nn_model_description_strings = model_description_strings[:num_nn_models]
 
-    assert len(nn_model_description_strings) == num_nn_models
-    nn_model_description_strings = [
-        s.replace('_', ' ') for s in nn_model_description_strings
-    ]
-
-    model_description_strings = (
-        nn_model_description_strings + BASELINE_DESCRIPTION_STRINGS
-    )
-    model_description_strings_fancy = (
-        nn_model_description_strings + BASELINE_DESCRIPTION_STRINGS_FANCY
-    )
-    top_model_dir_names = (
-        top_nn_model_dir_names + [''] * len(BASELINE_DESCRIPTION_STRINGS)
+    init_time_string = time_conversion.unix_sec_to_string(
+        init_time_unix_sec, TIME_FORMAT
     )
 
     nn_forecast_prob_matrix = numpy.array([])
@@ -248,11 +273,11 @@ def _run(top_nn_model_dir_names, nn_model_description_strings,
     for i in range(num_baseline_models):
         for this_x in x_tick_values:
             if BASELINE_DESCRIPTION_STRINGS[i] == 'basic':
-                label_string = 'R'
+                label_string = 'RII'
             elif BASELINE_DESCRIPTION_STRINGS[i] == 'consensus':
-                label_string = 'C'
+                label_string = 'Cons.'
             else:
-                label_string = 'D'
+                label_string = 'DTOPS'
 
             axes_object.plot(
                 this_x, baseline_forecast_probs[i], linestyle='None',
@@ -276,11 +301,169 @@ def _run(top_nn_model_dir_names, nn_model_description_strings,
     )
     axes_object.set_title(title_string)
 
-    output_file_name = '{0:s}/correlation_matrix.jpg'.format(output_dir_name)
-
     print('Saving figure to: "{0:s}"...'.format(output_file_name))
     figure_object.savefig(
         output_file_name, dpi=FIGURE_RESOLUTION_DPI,
+        pad_inches=0, bbox_inches='tight'
+    )
+    pyplot.close(figure_object)
+
+
+def _run(top_nn_model_dir_names, nn_model_description_strings,
+         norm_example_dir_name, normalization_file_name, cyclone_id_string,
+         init_time_string, output_dir_name):
+    """Plots case study with predictors, NN predictions, and baseline predictions.
+
+    This is effectively the main method.
+
+    :param top_nn_model_dir_names: See documentation at top of file.
+    :param nn_model_description_strings: Same.
+    :param norm_example_dir_name: Same.
+    :param normalization_file_name: Same.
+    :param cyclone_id_string: Same.
+    :param init_time_string: Same.
+    :param output_dir_name: Same.
+    """
+
+    init_time_unix_sec = time_conversion.string_to_unix_sec(
+        init_time_string, TIME_FORMAT
+    )
+
+    file_system_utils.mkdir_recursive_if_necessary(
+        directory_name=output_dir_name
+    )
+
+    num_nn_models = len(top_nn_model_dir_names)
+    num_baseline_models = len(BASELINE_DESCRIPTION_STRINGS)
+    num_models = num_nn_models + num_baseline_models
+
+    assert len(nn_model_description_strings) == num_nn_models
+    nn_model_description_strings = [
+        s.replace('_', ' ') for s in nn_model_description_strings
+    ]
+
+    model_description_strings = (
+        nn_model_description_strings + BASELINE_DESCRIPTION_STRINGS
+    )
+    model_description_strings_fancy = (
+        nn_model_description_strings + BASELINE_DESCRIPTION_STRINGS_FANCY
+    )
+    top_model_dir_names = (
+        top_nn_model_dir_names + [''] * len(BASELINE_DESCRIPTION_STRINGS)
+    )
+
+    _plot_forecast_probs(
+        top_model_dir_names=top_model_dir_names,
+        model_description_strings=model_description_strings,
+        cyclone_id_string=cyclone_id_string,
+        init_time_unix_sec=init_time_unix_sec,
+        output_file_name='{0:s}/forecast_probs.jpg'.format(output_dir_name)
+    )
+
+    norm_example_file_name = example_io.find_file(
+        directory_name=norm_example_dir_name,
+        cyclone_id_string=cyclone_id_string,
+        prefer_zipped=False, allow_other_format=True,
+        raise_error_if_missing=True
+    )
+
+    generator_option_dict = {
+        neural_net.EXAMPLE_FILE_KEY: norm_example_file_name,
+        neural_net.LEAD_TIMES_KEY: numpy.array([24], dtype=int),
+        neural_net.SATELLITE_PREDICTORS_KEY:
+            [satellite_utils.BRIGHTNESS_TEMPERATURE_KEY],
+        neural_net.SATELLITE_LAG_TIMES_KEY: numpy.array([0], dtype=int),
+        neural_net.SHIPS_GOES_LAG_TIMES_KEY: numpy.array([0], dtype=int),
+        neural_net.SHIPS_GOES_PREDICTORS_KEY: SHIPS_LAGGED_PREDICTOR_NAMES,
+        neural_net.SHIPS_FORECAST_PREDICTORS_KEY:
+            SHIPS_FORECAST_PREDICTOR_NAMES,
+        neural_net.SHIPS_MAX_FORECAST_HOUR_KEY: SHIPS_MAX_FORECAST_HOUR,
+        neural_net.NUM_POSITIVE_EXAMPLES_KEY: 2,
+        neural_net.NUM_NEGATIVE_EXAMPLES_KEY: 2,
+        neural_net.MAX_EXAMPLES_PER_CYCLONE_KEY: 2,
+        neural_net.PREDICT_TD_TO_TS_KEY: False,
+        neural_net.CLASS_CUTOFFS_KEY:
+            numpy.array([RAPID_INTENSIFN_CUTOFF_M_S01]),
+        neural_net.NUM_GRID_ROWS_KEY: 380,
+        neural_net.NUM_GRID_COLUMNS_KEY: 540,
+        neural_net.USE_TIME_DIFFS_KEY: False,
+        neural_net.SATELLITE_TIME_TOLERANCE_KEY: 86400,
+        neural_net.SATELLITE_MAX_MISSING_TIMES_KEY: 1,
+        neural_net.SHIPS_TIME_TOLERANCE_KEY: 0,
+        neural_net.SHIPS_MAX_MISSING_TIMES_KEY: 0,
+        neural_net.USE_CLIMO_KEY: False,
+        neural_net.DATA_AUG_NUM_TRANS_KEY: 0,
+        neural_net.DATA_AUG_NUM_ROTATIONS_KEY: 0,
+        neural_net.DATA_AUG_NUM_NOISINGS_KEY: 0
+    }
+
+    data_dict = neural_net.create_inputs(generator_option_dict)
+    predictor_matrices = data_dict[neural_net.PREDICTOR_MATRICES_KEY]
+    all_init_times_unix_sec = data_dict[neural_net.INIT_TIMES_KEY]
+
+    init_time_unix_sec = time_conversion.string_to_unix_sec(
+        init_time_string, TIME_FORMAT
+    )
+    time_index = general_utils.find_exact_times(
+        actual_times_unix_sec=all_init_times_unix_sec,
+        desired_times_unix_sec=numpy.array([init_time_unix_sec], dtype=int)
+    )[0]
+    predictor_matrices = [
+        None if m is None else m[[time_index], ...] for m in predictor_matrices
+    ]
+
+    forecast_hours = numpy.linspace(
+        0, SHIPS_MAX_FORECAST_HOUR,
+        num=int(numpy.round(SHIPS_MAX_FORECAST_HOUR / 6)) + 1,
+        dtype=int
+    )
+
+    (
+        figure_objects, axes_objects, _
+    ) = predictor_plotting.plot_lagged_ships_one_example(
+        predictor_matrices_one_example=predictor_matrices,
+        model_metadata_dict=
+        {neural_net.VALIDATION_OPTIONS_KEY: generator_option_dict},
+        cyclone_id_string=cyclone_id_string,
+        forecast_hours=forecast_hours,
+        init_time_unix_sec=init_time_unix_sec
+    )
+
+    figure_object = figure_objects[0]
+    axes_object = axes_objects[0]
+    axes_object.set_title('GOES-based SHIPS predictors')
+
+    panel_file_names = [
+        '{0:s}/goes_based_predictors.jpg'.format(output_dir_name)
+    ]
+    print('Saving figure to: "{0:s}"...'.format(panel_file_names[-1]))
+    figure_object.savefig(
+        panel_file_names[-1], dpi=FIGURE_RESOLUTION_DPI,
+        pad_inches=0, bbox_inches='tight'
+    )
+    pyplot.close(figure_object)
+
+    (
+        figure_objects, axes_objects, _
+    ) = predictor_plotting.plot_forecast_ships_one_example(
+        predictor_matrices_one_example=predictor_matrices,
+        model_metadata_dict=
+        {neural_net.VALIDATION_OPTIONS_KEY: generator_option_dict},
+        cyclone_id_string=cyclone_id_string,
+        forecast_hours=forecast_hours,
+        init_time_unix_sec=init_time_unix_sec
+    )
+
+    figure_object = figure_objects[0]
+    axes_object = axes_objects[0]
+    axes_object.set_title('Environmental and historical SHIPS predictors')
+
+    panel_file_names.append(
+        '{0:s}/enviro_and_hist_predictors.jpg'.format(output_dir_name)
+    )
+    print('Saving figure to: "{0:s}"...'.format(panel_file_names[-1]))
+    figure_object.savefig(
+        panel_file_names[-1], dpi=FIGURE_RESOLUTION_DPI,
         pad_inches=0, bbox_inches='tight'
     )
     pyplot.close(figure_object)
