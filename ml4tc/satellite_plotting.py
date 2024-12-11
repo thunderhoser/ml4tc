00804332e51b64a7d8252db17acf946121b6e5d4
thunderhoser <lagerqui@ualberta.ca>
2024-12-10 17:33:33 -0700

    FOO

--- ml4tc/satellite_plotting.py
+++ ml4tc/satellite_plotting.py
@@ -314,7 +314,8 @@ def plot_2d_grid(
             longitudes_to_plot_deg_e, latitudes_to_plot_deg_n,
             temp_matrix_to_plot_kelvins,
             cmap=colour_map_object, norm=colour_norm_object,
-            vmin=min_colour_value, vmax=max_colour_value, shading='flat',
+            # vmin=min_colour_value, vmax=max_colour_value,
+            shading='flat',
             edgecolors='None', zorder=-1e11
         )
     else:
